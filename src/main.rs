617095c81300fbea4e282663973d361489e56c15
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-02-07 23:01:40 +0200

    resolved issue with parsing args

--- src/main.rs
+++ src/main.rs
@@ -18,57 +18,67 @@ use crate::response::Response;
 use crate::server::Server;
 use crate::thread_pool::ThreadPool;
 
+struct Config<'a> {
+    static_dirs: &'a str,
+    port: usize,
+    ip: &'a str,
+    thread_count: usize,
+}
 
 fn main() {
     // to run execute following:
-    // cargo run 7777 localhost /static,/assets
+    // cargo run 7777 localhost /static,/assets 6
     // where
     // 7777 --> port
     // localhost --> ip
     // /static,/assets --> list of directories in root with static assets
+    // 6 --> thread count
 
-    // alternatively you can run built executable
+    // alternatively you can run built executable 12
     // rws 8888 127.0.0.1 /images,/assets
     // where
     // 8888 --> port
     // 127.0.0.1 --> ip
     // /images,/assets --> list of directories in root with static assets
+    // 12 --> thread count
+
+    const CONFIG: Config<'static> = Config {
+        static_dirs: "/static,/assets",
+        port: 7878,
+        ip: "127.0.0.1",
+        thread_count: 4
+    };
 
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let mut port = 7878;
     if args.len() >= 2 {
-        port = (&args[1]).parse().unwrap();
+        CONFIG.port = (&args[1]).parse().unwrap();
     }
 
-    let mut ip = "127.0.0.1";
     if args.len() >= 3 {
-        ip = &args[2];
+        CONFIG.ip = &args[2];
     }
-    let bind_addr = [ip, ":", &port.to_string()].join("");
 
-    let mut static_directories_args = "/static,/assets";
     if args.len() >= 4 {
-        //static_directories_args = &args[3];
+        CONFIG.static_dirs = &args[3];
     }
 
-    let mut threads_count = 4;
     if args.len() >= 5 {
-        threads_count = (&args[4]).parse().unwrap();
+        CONFIG.thread_count = (&args[4]).parse().unwrap();
     }
 
-
+    let bind_addr = [CONFIG.ip, ":", CONFIG.port.to_string().as_str()].join("");
     println!("Hello, rust-web-server! {}", bind_addr);
     let listener = TcpListener::bind(bind_addr).unwrap();
-    let pool = ThreadPool::new(threads_count);
+    let pool = ThreadPool::new(CONFIG.thread_count);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
 
-        pool.execute(move || {
-            Server::handle_connection(stream, static_directories_args);
+        pool.execute(move ||  {
+            Server::handle_connection(stream, CONFIG.static_dirs);
         });
     }
 }
