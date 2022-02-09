a83f70cd2897d00fa7914a76ea46111a0727b67f
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-02-09 13:54:45 +0200

    removed local unnecessary variables

--- src/mime_type.rs
+++ src/mime_type.rs
@@ -6,18 +6,16 @@ impl MimeType {
     pub(crate) const VIDEO_MP4: &'static str = "video/mp4";
 
     pub(crate) fn detect_mime_type(request_uri: &str) -> String {
-        const APPLICATION_OCTET_STREAM: &str = "application/octet-stream";
-        const VIDEO_MP4: &str = "video/mp4";
 
         let mp4_suffix = ".mp4";
 
         let is_video_mp4 = request_uri.ends_with(mp4_suffix);
 
         if is_video_mp4 {
-            return VIDEO_MP4.to_string();
+            return MimeType::VIDEO_MP4.to_string();
         }
 
-        return APPLICATION_OCTET_STREAM.to_string();
+        return MimeType::APPLICATION_OCTET_STREAM.to_string();
     }
 
 }
