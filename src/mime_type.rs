85fab49099be8eaa075f5fe359a37873f4cedc62
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-02-09 14:28:20 +0200

    added support for css mime type

--- src/mime_type.rs
+++ src/mime_type.rs
@@ -5,9 +5,11 @@ impl MimeType {
     pub(crate) const APPLICATION_OCTET_STREAM: &'static str = "application/octet-stream";
     pub(crate) const VIDEO_MP4: &'static str = "video/mp4";
     pub(crate) const TEXT_PLAIN: &'static str = "text/plain";
+    pub(crate) const TEXT_CSS: &'static str = "text/css";
 
     const MP4_SUFFIX: &'static str = ".mp4";
     const TXT_SUFFIX: &'static str = ".txt";
+    const CSS_SUFFIX: &'static str = ".css";
 
     pub(crate) fn detect_mime_type(request_uri: &str) -> String {
 
@@ -21,7 +23,10 @@ impl MimeType {
             return MimeType::TEXT_PLAIN.to_string();
         }
 
-
+        let is_css_suffix = request_uri.ends_with(MimeType::CSS_SUFFIX);
+        if is_css_suffix {
+            return MimeType::TEXT_CSS.to_string();
+        }
 
         return MimeType::APPLICATION_OCTET_STREAM.to_string();
     }
