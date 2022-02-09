018cadaa43d7e7664a22f9f5552338e45f2650cb
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-02-09 14:20:45 +0200

    added support for txt mime type

--- src/mime_type.rs
+++ src/mime_type.rs
@@ -4,16 +4,24 @@ pub struct MimeType {}
 impl MimeType {
     pub(crate) const APPLICATION_OCTET_STREAM: &'static str = "application/octet-stream";
     pub(crate) const VIDEO_MP4: &'static str = "video/mp4";
+    pub(crate) const TEXT_PLAIN: &'static str = "text/plain";
+
+    const MP4_SUFFIX: &'static str = ".mp4";
+    const TXT_SUFFIX: &'static str = ".txt";
 
     pub(crate) fn detect_mime_type(request_uri: &str) -> String {
 
-        let mp4_suffix = ".mp4";
-
-        let is_video_mp4 = request_uri.ends_with(mp4_suffix);
-
+        let is_video_mp4 = request_uri.ends_with(MimeType::MP4_SUFFIX);
         if is_video_mp4 {
             return MimeType::VIDEO_MP4.to_string();
         }
+
+        let is_txt_suffix = request_uri.ends_with(MimeType::TXT_SUFFIX);
+        if is_txt_suffix {
+            return MimeType::TEXT_PLAIN.to_string();
+        }
+
+
 
         return MimeType::APPLICATION_OCTET_STREAM.to_string();
     }
