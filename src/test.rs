b5c44da5b55c3b0faf03bf547212a8f24c7669b6
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-02-17 22:53:38 +0200

    added test to parse range

--- src/test.rs
+++ src/test.rs
@@ -1668,6 +1668,42 @@ mod tests {
     }
 
     #[test]
+    fn parse_range_test() {
+        let file_length = 2504382;
+
+        let byte = "200-1000";
+        let range = Range::parse_range(file_length, byte);
+        assert_eq!(range.start, 200);
+        assert_eq!(range.end, 1000);
+
+        let byte = " 1200-1400";
+        let range = Range::parse_range(file_length, byte);
+        assert_eq!(range.start, 1200);
+        assert_eq!(range.end, 1400);
+
+        let byte = " 2000-2300 ";
+        let range = Range::parse_range(file_length, byte);
+        assert_eq!(range.start, 2000);
+        assert_eq!(range.end, 2300);
+
+        let byte = "  11000- ";
+        let range = Range::parse_range(file_length, byte);
+        assert_eq!(range.start, 11000);
+        assert_eq!(range.end, file_length);
+
+        let byte = " -500 ";
+        let range = Range::parse_range(file_length, byte);
+        assert_eq!(range.start, file_length - 500);
+        assert_eq!(range.end, file_length);
+
+        let byte = " 0- ";
+        let range = Range::parse_range(file_length, byte);
+        assert_eq!(range.start, 0);
+        assert_eq!(range.end, file_length);
+
+    }
+
+    #[test]
     fn get_right_start_end_length_of_a_file() {
         let image_path = "/static/content.png";
 
