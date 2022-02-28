078b5bd1c9aefcee35f33eec8a6c443b4230009b
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-03-01 01:50:16 +0200

    wip

--- src/range.rs
+++ src/range.rs
@@ -4,13 +4,13 @@ use std::{env, fs, io};
 use std::borrow::Borrow;
 use std::char::MAX;
 use std::fs::{File, metadata};
-use std::io::{BufReader, SeekFrom};
+use std::io::{BufReader, Cursor, SeekFrom};
 
 use crate::request::Request;
 use crate::response::Response;
 use crate::app::App;
 use crate::{CONSTANTS, Server};
-use crate::constant::{HTTP_VERSIONS, REQUEST_METHODS, RESPONSE_STATUS_CODE_REASON_PHRASES};
+use crate::constant::{HTTP_HEADERS, HTTP_VERSIONS, REQUEST_METHODS, RESPONSE_STATUS_CODE_REASON_PHRASES};
 use crate::header::Header;
 use crate::mime_type::MimeType;
 
@@ -119,6 +119,144 @@ impl Range {
         if md.is_file() {
             content_range_list = Range::parse_content_range(&static_filepath, md.len(), &range.header_value);
         }
+
+        content_range_list
+    }
+
+    pub(crate) fn parse_multipart_body(cursor: &mut Cursor<&[u8]>, mut content_range_list: Vec<ContentRange>) -> Vec<ContentRange> {
+
+        let mut buf = vec![];
+        let bytes_offset = cursor.read_until(b'\n', &mut buf).unwrap();
+        let mut b : &[u8] = &buf;
+        let mut string = String::from_utf8(Vec::from(b)).unwrap();
+
+        let new_line_char_found = bytes_offset != 0;
+        let current_string_is_empty = string.trim().len() == 0;
+
+        // println!("string: {} new_line_char_found: {} current_string_is_empty: {}", string, new_line_char_found, current_string_is_empty);
+        println!("string: {}", string);
+
+        if !new_line_char_found {
+            println!("return content_range_list length: {}", content_range_list.len());
+            return content_range_list
+        };
+
+        let mut content_range: ContentRange = ContentRange {
+            unit: CONSTANTS.BYTES.to_string(),
+            range: Range { start: 0, end: 0 },
+            size: "".to_string(),
+            body: vec![],
+            content_type: "".to_string()
+        };
+
+        let content_range_is_not_parsed = content_range.body.len() == 0;
+        if string.starts_with(CONSTANTS.SEPARATOR) && content_range_is_not_parsed {
+            buf = vec![];
+            cursor.read_until(b'\n', &mut buf).unwrap();
+            b = &buf;
+            string = String::from_utf8(Vec::from(b)).unwrap();
+
+
+            println!("string: {}", string);
+        }
+
+        let content_type_is_not_parsed = content_range.content_type.len() == 0;
+        if string.starts_with(HTTP_HEADERS.CONTENT_TYPE) && content_type_is_not_parsed {
+            let content_type = Response::parse_http_response_header_string(string.as_str());
+            content_range.content_type = content_type.header_value.trim().to_string();
+
+            buf = vec![];
+            cursor.read_until(b'\n', &mut buf).unwrap();
+            b = &buf;
+            string = String::from_utf8(Vec::from(b)).unwrap();
+
+
+            println!("content type is {}", &content_range.content_type);
+            println!("string: {}", string);
+        }
+
+        let content_range_is_not_parsed = content_range.size.len() == 0;
+        if string.starts_with(HTTP_HEADERS.CONTENT_RANGE) && content_range_is_not_parsed {
+            let content_range_header = Response::parse_http_response_header_string(string.as_str());
+            //parse header value ...
+            let split_token = [CONSTANTS.BYTES, CONSTANTS.WHITESPACE].join("");
+            let first_split: Vec<&str> = content_range_header.header_value.split(&split_token).collect();
+
+            let value_index = 1;
+            let first_split_string = first_split.get(value_index).unwrap().trim();
+            //println!(": {}", &first_split_string);
+
+            let split_token = CONSTANTS.SLASH;
+            let second_split: Vec<&str> = first_split_string.split(split_token).collect();
+
+            let second_split_first_value = second_split.get(0).unwrap().trim();
+            let second_split_second_value = second_split.get(1).unwrap().trim();
+            content_range.size = second_split_second_value.to_string();
+            //println!(": {} : {}", &second_split_first_value, &second_split_second_value);
+
+            let split_token = CONSTANTS.HYPHEN;
+            let third_split : Vec<&str> = second_split_first_value.split(split_token).collect();
+            let third_split_first_value =  third_split.get(0).unwrap().trim();
+            let third_split_second_value =  third_split.get(1).unwrap().trim();
+            content_range.range.start = third_split_first_value.parse().unwrap();
+            content_range.range.end = third_split_second_value.parse().unwrap();
+            //println!(": {} : {}", &third_split_first_value, &third_split_second_value);
+
+
+
+            buf = vec![];
+            cursor.read_until(b'\n', &mut buf).unwrap();
+            b = &buf;
+            string = String::from_utf8(Vec::from(b)).unwrap();
+
+            println!("content range start is {} and end {}, size is {}", content_range.range.start, content_range.range.end, content_range.size);
+            println!("string: {}", string);
+
+            buf = vec![];
+            cursor.read_until(b'\n', &mut buf).unwrap();
+            b = &buf;
+            string = String::from_utf8(Vec::from(b)).unwrap();
+        }
+
+        let content_range_is_parsed = content_range.size.len() != 0;
+        let content_type_is_parsed = content_range.content_type.len() != 0;
+        if content_range_is_parsed && content_type_is_parsed {
+            let mut body : Vec<u8> = vec![];
+            // println!("before while {} body.len: {} string len: {}", string, body.len(), string.len());
+            body = [body, string.as_bytes().to_vec()].concat();
+
+            while !string.starts_with(CONSTANTS.SEPARATOR) {
+                buf = vec![];
+                cursor.read_until(b'\n', &mut buf).unwrap();
+                b = &buf;
+                string = String::from_utf8(Vec::from(b)).unwrap();
+
+                if !string.starts_with(CONSTANTS.SEPARATOR) {
+                    body = [body, string.as_bytes().to_vec()].concat();
+                }
+            }
+
+
+
+            let mut debug_body : &[u8]  = &body;
+            println!("content range body is {} length is {}", String::from_utf8(debug_body.to_vec()).unwrap(), debug_body.len());
+
+            content_range.body = body;
+
+            let size_is_known = content_range.size != "*";
+            let range_end_is_bigger_than_filesize = size_is_known && content_range.range.end <= content_range.size.parse().unwrap();
+            if range_end_is_bigger_than_filesize {
+                //status.is_ok = false;
+                //status.message = "content range end is bigger than filesize".to_string();
+            }
+
+            content_range_list.push(content_range);
+        }
+
+        // println!("!!! {} {} {} {} {} {}", content_range.unit, content_range.content_type, content_range.size, content_range.range.start, content_range.range.end, content_range.body.len());
+
+        println!("content_range_list length: {}", content_range_list.len());
+        content_range_list = Range::parse_multipart_body(cursor, content_range_list);
 
         content_range_list
     }
