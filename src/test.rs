addf9668e2bf7adc6f21a849468a80cdc968e320
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-03-02 01:39:13 +0200

    wip

--- src/test.rs
+++ src/test.rs
@@ -3,23 +3,3 @@ mod response_test;
 mod request_test;
 mod mime_type_test;
 mod server_test;
-
-use std::{env, fs};
-use regex::Regex;
-
-
-#[cfg(test)]
-mod tests {
-    use std::borrow::Borrow;
-    use std::fs::{File, metadata};
-    use std::io::{BufReader, Read, Seek, SeekFrom};
-    use crate::CONSTANTS;
-    use crate::constant::{HTTP_HEADERS, HTTP_VERSIONS, REQUEST_METHODS, RESPONSE_STATUS_CODE_REASON_PHRASES};
-    use crate::header::Header;
-    use crate::mime_type::MimeType;
-    use crate::range::{ContentRange, Range};
-    use crate::request::Request;
-    use crate::response::Response;
-    use crate::server::Server;
-    use super::*;
-}
