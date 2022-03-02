b61daec0d0cd0e3a3ed8a8b2a97b2e51d96cf216
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-03-02 03:18:37 +0200

    wip

--- src/response.rs
+++ src/response.rs
@@ -224,6 +224,8 @@ impl Response {
                 let mut range_list = vec![];
                 if boxed_value.is_ok() {
                     range_list = boxed_value.unwrap();
+                } else {
+                    panic!("unable to parse!")
                 }
 
                 response.content_range_list = range_list;
