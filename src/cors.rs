4556cd099219f787302046da2434010847d4eee0
Bohdan Mykolayovych Tsap <bohdaq@gmail.com>
2022-03-06 20:19:48 +0200

    wip

--- src/cors.rs
+++ src/cors.rs
@@ -17,52 +17,54 @@ impl Cors {
                 header_value: origin.unwrap().header_value.to_string()
             };
             response.headers.push(allow_origin);
-        }
 
-        let allow_credentials = Header {
-            header_name: Header::ACCESS_CONTROL_ALLOW_CREDENTIALS.to_string(),
-            header_value: "true".to_string()
-        };
-        response.headers.push(allow_credentials);
-
-        let vary = Header {
-            header_name: Header::VARY.to_string(),
-            header_value: Header::ORIGIN.to_string()
-        };
-        response.headers.push(vary);
-
-        let is_options = request.method == REQUEST_METHODS.OPTIONS;
-        if is_options {
-            let method = request.get_header(Header::ACCESS_CONTROL_REQUEST_METHOD.to_string());
-            if method.is_some() {
-                let allow_method = Header {
-                    header_name: Header::ACCESS_CONTROL_ALLOW_METHODS.to_string(),
-                    header_value: method.unwrap().header_value.to_string()
-                };
-                response.headers.push(allow_method);
-            }
-
-            let headers = request.get_header(Header::ACCESS_CONTROL_REQUEST_HEADERS.to_string());
-            if headers.is_some() {
-                let request_headers = headers.unwrap();
-                let allow_headers = Header {
-                    header_name: Header::ACCESS_CONTROL_ALLOW_HEADERS.to_string(),
-                    header_value: request_headers.header_value.to_string()
-                };
-                response.headers.push(allow_headers);
-
-                let expose_headers = Header {
-                    header_name: Header::ACCESS_CONTROL_EXPOSE_HEADERS.to_string(),
-                    header_value: request_headers.header_value.to_string()
-                };
-                response.headers.push(expose_headers);
-            }
-
-            let max_age = Header {
-                header_name: Header::ACCESS_CONTROL_MAX_AGE.to_string(),
-                header_value: Cors::MAX_AGE.to_string()
+            let allow_credentials = Header {
+                header_name: Header::ACCESS_CONTROL_ALLOW_CREDENTIALS.to_string(),
+                header_value: "true".to_string()
             };
-            response.headers.push(max_age);
+            response.headers.push(allow_credentials);
+
+            let vary = Header {
+                header_name: Header::VARY.to_string(),
+                header_value: Header::ORIGIN.to_string()
+            };
+            response.headers.push(vary);
+
+
+            let is_options = request.method == REQUEST_METHODS.OPTIONS;
+            if is_options {
+                let method = request.get_header(Header::ACCESS_CONTROL_REQUEST_METHOD.to_string());
+                if method.is_some() {
+                    let allow_method = Header {
+                        header_name: Header::ACCESS_CONTROL_ALLOW_METHODS.to_string(),
+                        header_value: method.unwrap().header_value.to_string()
+                    };
+                    response.headers.push(allow_method);
+                }
+
+                let headers = request.get_header(Header::ACCESS_CONTROL_REQUEST_HEADERS.to_string());
+                if headers.is_some() {
+                    let request_headers = headers.unwrap();
+                    let allow_headers = Header {
+                        header_name: Header::ACCESS_CONTROL_ALLOW_HEADERS.to_string(),
+                        header_value: request_headers.header_value.to_string()
+                    };
+                    response.headers.push(allow_headers);
+
+                    let expose_headers = Header {
+                        header_name: Header::ACCESS_CONTROL_EXPOSE_HEADERS.to_string(),
+                        header_value: request_headers.header_value.to_string()
+                    };
+                    response.headers.push(expose_headers);
+                }
+
+                let max_age = Header {
+                    header_name: Header::ACCESS_CONTROL_MAX_AGE.to_string(),
+                    header_value: Cors::MAX_AGE.to_string()
+                };
+                response.headers.push(max_age);
+            }
+
         }
 
         Ok((request, response))
