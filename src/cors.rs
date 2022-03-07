51d3ee50ff8ea3470ff33c3cf20501180ca71019
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-03-07 02:23:29 +0200

    wip

--- src/cors.rs
+++ src/cors.rs
@@ -3,7 +3,12 @@ use crate::constant::{HTTPError, REQUEST_METHODS};
 use crate::header::Header;
 
 pub struct Cors {
-
+    pub(crate) allow_origins: Vec<String>,
+    pub(crate) allow_methods: Vec<String>,
+    pub(crate) allow_headers: Vec<String>,
+    pub(crate) allow_credentials: bool,
+    pub(crate) expose_headers: Vec<String>,
+    pub(crate) max_age: String,
 }
 
 impl Cors {
@@ -66,6 +71,57 @@ impl Cors {
             }
 
         }
+
+        Ok((request, response))
+    }
+
+    pub(crate) fn process(request: Request, mut response: Response, cors: Cors) -> Result<(Request, Response), HTTPError> {
+
+        let origin = cors.allow_origins.join(", ");
+        let allow_origin = Header {
+            header_name: Header::ACCESS_CONTROL_ALLOW_ORIGIN.to_string(),
+            header_value: origin
+        };
+        response.headers.push(allow_origin);
+
+        let allow_credentials = Header {
+            header_name: Header::ACCESS_CONTROL_ALLOW_CREDENTIALS.to_string(),
+            header_value: cors.allow_credentials.to_string()
+        };
+        response.headers.push(allow_credentials);
+
+        let vary = Header {
+            header_name: Header::VARY.to_string(),
+            header_value: Header::ORIGIN.to_string()
+        };
+        response.headers.push(vary);
+
+        let methods = cors.allow_methods.join(", ");
+        let allow_methods = Header {
+            header_name: Header::ACCESS_CONTROL_ALLOW_METHODS.to_string(),
+            header_value: methods
+        };
+        response.headers.push(allow_methods);
+
+        let headers = cors.allow_headers.join(", ");
+        let allow_headers = Header {
+            header_name: Header::ACCESS_CONTROL_ALLOW_HEADERS.to_string(),
+            header_value: headers
+        };
+        response.headers.push(allow_headers);
+
+        let allow_expose_headers  = cors.expose_headers.join(", ");
+        let expose_headers = Header {
+            header_name: Header::ACCESS_CONTROL_EXPOSE_HEADERS.to_string(),
+            header_value: allow_expose_headers
+        };
+        response.headers.push(expose_headers);
+
+        let max_age = Header {
+            header_name: Header::ACCESS_CONTROL_MAX_AGE.to_string(),
+            header_value: Cors::MAX_AGE.to_string()
+        };
+        response.headers.push(max_age);
 
         Ok((request, response))
     }
