211fe71b7863048c1357480f203f37224bb0e8da
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-03-07 02:10:01 +0200

    wip

--- src/test/cors_test.rs
+++ src/test/cors_test.rs
@@ -43,7 +43,13 @@ fn cors_options_preflight_request() {
         header_value: request_access_control_request_headers_header_value.to_string()
     };
 
-    let headers = vec![host, origin, access_control_request_method, access_control_request_headers];
+    // aplication/json content type header makes this request follow the regular CORS flow
+    let content_type = Header {
+        header_name: Header::CONTENT_TYPE.to_string(),
+        header_value: MimeType::APPLICATION_JSON.to_string()
+    };
+
+    let headers = vec![host, origin, access_control_request_method, access_control_request_headers, content_type];
     let request = Request {
         method: request_method.to_string(),
         request_uri: request_uri.to_string(),
@@ -120,20 +126,34 @@ fn cors_options_preflight_request() {
 #[test]
 fn it_generates_successful_response_with_static_file() {
     // request test data
-    let request_host_header_name = Header::HOST;
-    let request_host_header_value = "localhost:7777";
+
     let request_method = REQUEST_METHODS.GET;
     let request_uri = "/static/test.json";
     let request_http_version = HTTP_VERSIONS.HTTP_VERSION_1_1.to_string();
 
 
-    // request part
+    let request_host_header_name = Header::HOST;
+    let request_host_header_value = "localhost:7777";
     let host = Header {
         header_name: request_host_header_name.to_string(),
         header_value: request_host_header_value.to_string()
     };
 
-    let headers = vec![host];
+    // aplication/json content type header makes this request follow the regular CORS flow
+    let content_type = Header {
+        header_name: Header::CONTENT_TYPE.to_string(),
+        header_value: MimeType::APPLICATION_JSON.to_string()
+    };
+
+    let request_origin_header_name = Header::ORIGIN;
+    let request_origin_header_value = "https://foo.example";
+    let origin = Header {
+        header_name: request_origin_header_name.to_string(),
+        header_value: request_origin_header_value.to_string()
+    };
+
+    let headers = vec![host, content_type, origin];
+
     let request = Request {
         method: request_method.to_string(),
         request_uri: request_uri.to_string(),
@@ -171,17 +191,25 @@ fn it_generates_successful_response_with_static_file() {
     };
     let raw_response: Vec<u8> = Server::process_request(mock_tcp_stream);
     let response = Response::parse_response(raw_response.borrow());
-    let header = response.get_header(response_content_length_header_name.to_string()).unwrap();
 
-    let content_type_header = response.get_header(Header::CONTENT_TYPE.to_string()).unwrap();
-    let x_content_type_options_header = response.get_header(Header::X_CONTENT_TYPE_OPTIONS.to_string()).unwrap();
-
-    assert_eq!(CONSTANTS.NOSNIFF, x_content_type_options_header.header_value);
-    assert_eq!(MimeType::APPLICATION_JSON, content_type_header.header_value);
-
-    assert_eq!(response_content_length_header_value, header.header_value);
     assert_eq!(response_http_version, response.http_version);
     assert_eq!(response_status_code, response.status_code);
     assert_eq!(response_reason_phrase, response.reason_phrase);
     assert_eq!(response_html_file.into_bytes(), response.content_range_list.get(0).unwrap().body);
+
+    let header = response.get_header(response_content_length_header_name.to_string()).unwrap();
+    assert_eq!(response_content_length_header_value, header.header_value);
+
+    let content_type_header = response.get_header(Header::CONTENT_TYPE.to_string()).unwrap();
+    assert_eq!(MimeType::APPLICATION_JSON, content_type_header.header_value);
+
+    let x_content_type_options_header = response.get_header(Header::X_CONTENT_TYPE_OPTIONS.to_string()).unwrap();
+    assert_eq!(CONSTANTS.NOSNIFF, x_content_type_options_header.header_value);
+
+    let vary_header = response.get_header(Header::VARY.to_string()).unwrap();
+    assert_eq!(Header::ORIGIN, vary_header.header_value);
+
+    let access_control_allow_origin_header = response.get_header(Header::ACCESS_CONTROL_ALLOW_ORIGIN.to_string()).unwrap();
+    assert_eq!(request_origin_header_value, access_control_allow_origin_header.header_value);
+
 }
