40f1844407c98bee46e7699fcdc7da1e581e6454
Bohdan Mykolayovych Tsap <bohdaq@gmail.com>
2022-03-10 21:04:15 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -129,15 +129,15 @@ fn is_config_file_provided(is_test_mode: bool) -> bool {
     println!("Start of Config Section");
     println!("Is Test Mode: {}", is_test_mode);
 
-    let mut filepath = "/config.toml";
+    let mut filepath = "/rws.config.toml";
     if is_test_mode {
-        filepath = "/src/test/config.toml"
+        filepath = "/src/test/rws.config.toml"
     }
     let static_filepath = Server::get_static_filepath(filepath);
     let mut is_config_provided = metadata(&static_filepath).is_ok();
 
     if !is_config_provided {
-        println!("config.toml is not provided");
+        println!("rws.config.toml is not provided");
         println!("End of Config Section");
 
     } else {
@@ -163,48 +163,48 @@ fn override_environment_variables_from_config(is_test_mode: bool) {
         }
     };
 
-    let mut filepath = "/config.toml";
+    let mut filepath = "/rws.config.toml";
     if is_test_mode {
-        filepath = "/src/test/config.toml"
+        filepath = "/src/test/rws.config.toml"
     }
     let static_filepath = Server::get_static_filepath(filepath);
     let content = std::fs::read_to_string(static_filepath);
 
     if content.is_err() {
-        println!("Unable to parse config.toml\n{}", content.err().unwrap());
+        println!("Unable to parse rws.config.toml\n{}", content.err().unwrap());
     } else {
         config = toml::from_str(content.unwrap().as_str()).unwrap();
     }
 
     env::set_var(Config::RWS_CONFIG_IP, config.ip.to_string());
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_IP, config.ip.to_string());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_IP, config.ip.to_string());
 
     env::set_var(Config::RWS_CONFIG_PORT, config.port.to_string());
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_PORT, config.port.to_string());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_PORT, config.port.to_string());
 
     env::set_var(Config::RWS_CONFIG_THREAD_COUNT, config.thread_count.to_string());
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_THREAD_COUNT, config.thread_count.to_string());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_THREAD_COUNT, config.thread_count.to_string());
 
     env::set_var(Config::RWS_CONFIG_CORS_ALLOW_ALL, config.cors.allow_all.to_string());
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_CORS_ALLOW_ALL, config.cors.allow_all.to_string());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_ALLOW_ALL, config.cors.allow_all.to_string());
 
     env::set_var(Config::RWS_CONFIG_CORS_ALLOW_ORIGINS, config.cors.allow_origins.join(","));
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_CORS_ALLOW_ORIGINS, config.cors.allow_origins.join(","));
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_ALLOW_ORIGINS, config.cors.allow_origins.join(","));
 
     env::set_var(Config::RWS_CONFIG_CORS_ALLOW_CREDENTIALS, config.cors.allow_credentials.to_string());
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_CORS_ALLOW_CREDENTIALS, config.cors.allow_credentials.to_string());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_ALLOW_CREDENTIALS, config.cors.allow_credentials.to_string());
 
     env::set_var(Config::RWS_CONFIG_CORS_ALLOW_HEADERS, config.cors.allow_headers.join(","));
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_CORS_ALLOW_HEADERS, config.cors.allow_headers.join(",").to_lowercase());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_ALLOW_HEADERS, config.cors.allow_headers.join(",").to_lowercase());
 
     env::set_var(Config::RWS_CONFIG_CORS_ALLOW_METHODS, config.cors.allow_methods.join(","));
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_CORS_ALLOW_METHODS, config.cors.allow_methods.join(","));
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_ALLOW_METHODS, config.cors.allow_methods.join(","));
 
     env::set_var(Config::RWS_CONFIG_CORS_EXPOSE_HEADERS, config.cors.expose_headers.join(","));
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_CORS_EXPOSE_HEADERS, config.cors.expose_headers.join(",").to_lowercase());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_EXPOSE_HEADERS, config.cors.expose_headers.join(",").to_lowercase());
 
     env::set_var(Config::RWS_CONFIG_CORS_MAX_AGE, &config.cors.max_age);
-    println!("Set env variable '{}' to value '{}' from config.toml", Config::RWS_CONFIG_CORS_MAX_AGE, config.cors.max_age);
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_MAX_AGE, config.cors.max_age);
 
     println!("End of Config Section");
 }
