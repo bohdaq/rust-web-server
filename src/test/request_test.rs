8a32eae60e9f6f972f786754e79bed2931c90366
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-04-05 16:56:34 +0300

    added test_request_ok

--- src/test/request_test.rs
+++ src/test/request_test.rs
@@ -20,3 +20,19 @@ fn method_and_request_uri_and_http_version_regex() {
     assert_eq!("/draho-brat_pt2/drahobrat_pt2_ver2.mp4", &caps["request_uri"]);
 
 }
+
+#[test]
+fn test_request_ok() {
+    let method = REQUEST_METHODS.GET;
+    let request_uri = CONSTANTS.SLASH;
+    let http_version = HTTP_VERSIONS.HTTP_VERSION_1_1;
+
+    let request_data = [method, request_uri, http_version].join(" ");
+    let raw_request = [request_data, CONSTANTS.NEW_LINE_SEPARATOR.to_string()].join("");
+
+    let request = Request::parse_request(raw_request.as_bytes());
+
+    assert_eq!(method, request.method);
+    assert_eq!(request_uri, request.request_uri);
+    assert_eq!(http_version, request.http_version);
+}
