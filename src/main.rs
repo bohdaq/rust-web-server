efbb65a15c16a4bf288bb0b13cc5d55f5500216a
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-09-18 03:00:00 +0300

    remoed mio dependency

--- src/main.rs
+++ src/main.rs
@@ -12,47 +12,35 @@ mod cors;
 
 extern crate core;
 
-use std::env;
-use std::fs::metadata;
-
+use std::net::TcpListener;
+use std::{env, thread};
 use std::collections::HashMap;
-use std::io::{self, Error, Read, Write};
-use std::net::Shutdown;
-use std::str::from_utf8;
-use std::{thread, time};
-use std::time::Instant;
+use std::fs::metadata;
+use std::time::Duration;
 
-use mio::event::{Event, Source};
-use mio::net::{TcpListener, TcpStream};
-use mio::{Events, Interest, Poll, Registry, Token};
-
-use crate::constant::{CONSTANTS, HTTP_VERSIONS, HTTPError, RESPONSE_STATUS_CODE_REASON_PHRASES, StatusCodeReasonPhrase};
+use crate::constant::CONSTANTS;
 
 use crate::request::Request;
 use crate::response::Response;
 use crate::server::Server;
 use crate::thread_pool::ThreadPool;
 
-use clap::Arg;
-use clap::App as ClapApp;
-use crate::app::App;
+use clap::{Arg, App};
 use serde::{Serialize, Deserialize};
 use crate::cors::Cors;
-use crate::mime_type::MimeType;
-use crate::range::{ContentRange, Range};
-
-const SERVER: Token = Token(0);
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     ip: String,
     port: i32,
+    thread_count: i32,
     cors: Cors,
 }
 
 impl Config {
     pub(crate) const RWS_CONFIG_IP: &'static str = "RWS_CONFIG_IP";
     pub(crate) const RWS_CONFIG_PORT: &'static str = "RWS_CONFIG_PORT";
+    pub(crate) const RWS_CONFIG_THREAD_COUNT: &'static str = "RWS_CONFIG_THREAD_COUNT";
     pub(crate) const RWS_CONFIG_CORS_ALLOW_ALL: &'static str = "RWS_CONFIG_CORS_ALLOW_ALL";
     pub(crate) const RWS_CONFIG_CORS_ALLOW_ORIGINS: &'static str = "RWS_CONFIG_CORS_ALLOW_ORIGINS";
     pub(crate) const RWS_CONFIG_CORS_ALLOW_CREDENTIALS: &'static str = "RWS_CONFIG_CORS_ALLOW_CREDENTIALS";
@@ -63,17 +51,16 @@ impl Config {
 
     pub(crate) const RWS_DEFAULT_IP: &'static str = "127.0.0.1";
     pub(crate) const RWS_DEFAULT_PORT: &'static i32 = &7878;
+    pub(crate) const RWS_DEFAULT_THREAD_COUNT: &'static i32 = &4;
 
 }
 
 fn main() {
-    //test plan: test allowed request with config via system vars, config file or command line
-    //           test misconfigured origin, header, presence allow credentials
     let is_test_mode = false;
 
     bootstrap(is_test_mode);
-    let (ip, port) = get_ip_port();
-    create_tcp_listener(ip.as_str(), port);
+    let (ip, port, thread_count) = get_ip_port_thread_count();
+    create_tcp_listener_with_thread_pool(ip.as_str(), port, thread_count);
 }
 
 fn bootstrap(is_test_mode: bool) {
@@ -98,6 +85,11 @@ fn read_system_environment_variables() {
     let boxed_port = env::var(Config::RWS_CONFIG_PORT);
     if boxed_port.is_ok() {
         println!("{}={}", Config::RWS_CONFIG_PORT, boxed_port.unwrap());
+    }
+
+    let boxed_thread_count = env::var(Config::RWS_CONFIG_THREAD_COUNT);
+    if boxed_thread_count.is_ok() {
+        println!("{}={}", Config::RWS_CONFIG_THREAD_COUNT, boxed_thread_count.unwrap());
     }
 
     let boxed_cors_allow_all = env::var(Config::RWS_CONFIG_CORS_ALLOW_ALL);
@@ -164,6 +156,7 @@ fn override_environment_variables_from_config(is_test_mode: bool) {
     let mut config: Config = Config {
         ip: "".to_string(),
         port: 0,
+        thread_count: 0,
         cors: Cors {
             allow_all: false,
             allow_origins: vec![],
@@ -194,6 +187,9 @@ fn override_environment_variables_from_config(is_test_mode: bool) {
     env::set_var(Config::RWS_CONFIG_PORT, config.port.to_string());
     println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_PORT, config.port.to_string());
 
+    env::set_var(Config::RWS_CONFIG_THREAD_COUNT, config.thread_count.to_string());
+    println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_THREAD_COUNT, config.thread_count.to_string());
+
     env::set_var(Config::RWS_CONFIG_CORS_ALLOW_ALL, config.cors.allow_all.to_string());
     println!("Set env variable '{}' to value '{}' from rws.config.toml", Config::RWS_CONFIG_CORS_ALLOW_ALL, config.cors.allow_all.to_string());
 
@@ -222,7 +218,7 @@ fn override_environment_variables_from_command_line_args() {
     println!("Start of Reading Command Line Arguments");
 
     const VERSION: &str = env!("CARGO_PKG_VERSION");
-    let matches = ClapApp::new("rws rust-web-server")
+    let matches = App::new("rws rust-web-server")
         .version(VERSION)
         .author("Bohdan Tsap <bohdan.tsap@tutanota.com>")
         .about("Hi, rust-web-server (rws) is a simple web-server written in Rust. The rws server can serve static content inside the directory it is started.")
@@ -236,6 +232,11 @@ fn override_environment_variables_from_command_line_args() {
             .long("ip")
             .takes_value(true)
             .help("IP or domain"))
+        .arg(Arg::new("threads")
+            .short('t')
+            .long("threads")
+            .takes_value(true)
+            .help("Number of threads"))
         .arg(Arg::new("cors-allow-all")
             .short('a')
             .long("cors-allow-all")
@@ -293,6 +294,20 @@ fn override_environment_variables_from_command_line_args() {
         Some(ip) => {
             env::set_var(Config::RWS_CONFIG_IP, ip.to_string());
             println!("Set env variable '{}' to value '{}' from command line argument", Config::RWS_CONFIG_IP, ip.to_string());
+        }
+    }
+
+    let threads_match = matches.value_of("threads");
+    match threads_match {
+        None => print!(""),
+        Some(s) => {
+            match s.parse::<i32>() {
+                Ok(thread_count) => {
+                    env::set_var(Config::RWS_CONFIG_THREAD_COUNT, thread_count.to_string());
+                    println!("Set env variable '{}' to value '{}' from command line argument", Config::RWS_CONFIG_THREAD_COUNT, thread_count.to_string());
+                },
+                Err(_) => println!("That's not a number! {}", s),
+            }
         }
     }
 
@@ -364,9 +379,10 @@ fn override_environment_variables_from_command_line_args() {
     println!("End of Reading Command Line Arguments");
 }
 
-fn get_ip_port() -> (String, i32) {
+fn get_ip_port_thread_count() -> (String, i32, i32) {
     let mut ip : String = Config::RWS_DEFAULT_IP.to_string();
     let mut port: i32 = *Config::RWS_DEFAULT_PORT;
+    let mut thread_count: i32 = *Config::RWS_DEFAULT_THREAD_COUNT;
 
     let boxed_ip = env::var(Config::RWS_CONFIG_IP);
     if boxed_ip.is_ok() {
@@ -378,217 +394,28 @@ fn get_ip_port() -> (String, i32) {
         port = boxed_port.unwrap().parse().unwrap()
     }
 
-    (ip, port)
-}
-
-fn create_tcp_listener(ip: &str, port: i32) -> io::Result<()> {
-    let bind_addr = [ip, ":", port.to_string().as_str()].join(CONSTANTS.EMPTY_STRING);
-
-    // Create a poll instance.
-    let mut poll = Poll::new()?;
-    // Create storage for events.
-    let mut events = Events::with_capacity(10000);
-
-    // Setup the TCP server socket.
-    let addr = bind_addr.parse().unwrap();
-    let boxed_tcp_listener = TcpListener::bind(addr);
-    if boxed_tcp_listener.is_err() {
-        println!("Error: Unable to bind {}\nTo check what process is using port try to run 'lsof -i :PORT_NUMBER'\nTo get information about the running process try to run 'ps -p PID'", bind_addr);
+    let boxed_thread_count = env::var(Config::RWS_CONFIG_THREAD_COUNT);
+    if boxed_thread_count.is_ok() {
+        thread_count = boxed_thread_count.unwrap().parse().unwrap()
     }
 
-    let mut server = boxed_tcp_listener.unwrap();
+    (ip, port, thread_count)
+}
+
+fn create_tcp_listener_with_thread_pool(ip: &str, port: i32, thread_count: i32) {
+    let bind_addr = [ip, ":", port.to_string().as_str()].join(CONSTANTS.EMPTY_STRING);
     println!("Hello, rust-web-server is up and running: {}", bind_addr);
 
-
-    // Register the server with poll we can receive events for it.
-    poll.registry()
-        .register(&mut server, SERVER, Interest::READABLE.add(Interest::WRITABLE))?;
-
-    // Map of `Token` -> `TcpStream`.
-    let mut connections = HashMap::new();
-    // Unique token for each incoming connection.
-    let mut unique_token = Token(SERVER.0 + 1);
+    let listener = TcpListener::bind(bind_addr).unwrap();
+    let pool = ThreadPool::new(thread_count as usize);
 
 
-    loop {
-        poll.poll(&mut events, None)?;
+    for boxed_stream in listener.incoming() {
+        let stream = boxed_stream.unwrap();
+        println!("Connection established, local addr: {}, peer addr: {}", stream.local_addr().unwrap(), stream.peer_addr().unwrap());
 
-        for event in events.iter() {
-            let now = Instant::now();
-            match event.token() {
-                SERVER => loop {
-                    // Received an event for the TCP server socket, which
-                    // indicates we can accept an connection.
-                    let (mut connection, address) = match server.accept() {
-                        Ok((connection, address)) => (connection, address),
-                        Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
-                            // If we get a `WouldBlock` error we know our
-                            // listener has no more incoming connections queued,
-                            // so we can return to polling and wait for some
-                            // more.
-                            break;
-                        }
-                        Err(e) => {
-                            // If it was any other kind of error, something went
-                            // wrong and we terminate with an error.
-                            return Err(e);
-                        }
-                    };
-
-                    println!("\n\n\nAccepted connection from: {}", address);
-
-                    let token = next(&mut unique_token);
-                    poll.registry().register(
-                        &mut connection,
-                        token,
-                        Interest::READABLE.add(Interest::WRITABLE),
-                    )?;
-
-                    connections.insert(token, connection);
-                },
-                token => {
-                    // Maybe received an event for a TCP connection.
-                    let done = if let Some(connection) = connections.get_mut(&token) {
-                        handle_connection_event(poll.registry(), connection, event, now)?
-                    } else {
-                        // Sporadic events happen, we can safely ignore them.
-                        false
-                    };
-                    if done {
-                        if let Some(mut connection) = connections.remove(&token) {
-                            poll.registry().deregister(&mut connection)?;
-                        }
-                    }
-                }
-            }
-        }
+        pool.execute(move ||  {
+            Server::process_request(stream);
+        });
     }
-
-}
-
-
-fn next(current: &mut Token) -> Token {
-    let next = current.0;
-    current.0 += 1;
-    Token(next)
-}
-
-/// Returns `true` if the connection is done.
-fn handle_connection_event(
-    registry: &Registry,
-    connection: &mut TcpStream,
-    event: &Event,
-    now: Instant,
-) -> io::Result<bool> {
-
-    let mut connection_closed = false;
-
-
-    let mut received_data = vec![0; 4096];
-    let mut bytes_read = 0;
-    // We can (maybe) read from the connection.
-    loop {
-        match connection.read(&mut received_data[bytes_read..]) {
-            Ok(0) => {
-                // Reading 0 bytes means the other side has closed the
-                // connection or is done writing, then so are we.
-                connection_closed = true;
-                break;
-            }
-            Ok(n) => {
-                bytes_read += n;
-                if bytes_read == received_data.len() {
-                    received_data.resize(received_data.len() + 1024, 0);
-                }
-            }
-            // Would block "errors" are the OS's way of saying that the
-            // connection is not actually ready to perform this I/O operation.
-            Err(ref err) if would_block(err) => break,
-            Err(ref err) if interrupted(err) => continue,
-            // Other errors we'll consider fatal.
-            Err(err) => return Err(err),
-        }
-    }
-
-    let mut raw_response : Vec<u8> = vec![];
-
-    if bytes_read != 0 {
-        let received_data = &received_data[..bytes_read];
-        println!("Read {} bytes", received_data.len());
-
-        let boxed_request = Request::parse_request(received_data.as_ref());
-        if boxed_request.is_ok() {
-            let request = boxed_request.unwrap();
-            let (response, request) = App::handle_request(request);
-            raw_response = Response::generate_response(response, request);
-        } else {
-            let error = boxed_request.err().unwrap();
-            let content_range = ContentRange {
-                unit: CONSTANTS.BYTES.to_string(),
-                range: Range {
-                    start: 0,
-                    end: error.len() as u64
-                },
-                size: error.len().to_string(),
-                body: error.into_bytes(),
-                content_type: MimeType::TEXT_PLAIN.to_string()
-            };
-            let bad_request_error_response = Response {
-                http_version: HTTP_VERSIONS.HTTP_VERSION_1_1.to_string(),
-                status_code: RESPONSE_STATUS_CODE_REASON_PHRASES.N400_BAD_REQUEST.STATUS_CODE.to_string(),
-                reason_phrase: RESPONSE_STATUS_CODE_REASON_PHRASES.N400_BAD_REQUEST.REASON_PHRASE.to_string(),
-                headers: vec![],
-                content_range_list: vec![content_range]
-            };
-            raw_response = Response::generate_response(bad_request_error_response, Request {
-                method: "".to_string(),
-                request_uri: "".to_string(),
-                http_version: "".to_string(),
-                headers: vec![]
-            });
-
-        }
-
-    }
-
-
-    // We can (maybe) write to the connection.
-
-    match connection.write(raw_response.as_ref()) {
-        // We want to write the entire `DATA` buffer in a single go. If we
-        // write less we'll return a short write error (same as
-        // `io::Write::write_all` does).
-        Ok(n) if n < raw_response.len() => return Err(io::ErrorKind::WriteZero.into()),
-        Ok(_) => {
-            // After we've written something we'll reregister the connection
-            // to only respond to readable events.
-            registry.reregister(connection, event.token(), Interest::READABLE)?;
-
-            println!("Written {} bytes", raw_response.len());
-        }
-        // Would block "errors" are the OS's way of saying that the
-        // connection is not actually ready to perform this I/O operation.
-        Err(ref err) if would_block(err) => {}
-        // Got interrupted (how rude!), we'll try again.
-        Err(ref err) if interrupted(err) => {
-            return handle_connection_event(registry, connection, event, now)
-        }
-        // Other errors we'll consider fatal.
-        Err(err) => return Err(err),
-    }
-
-    if connection_closed {
-        println!("Connection closed. Processing took {} milliseconds", now.elapsed().as_millis());
-        return Ok(true);
-    }
-
-    Ok(false)
-}
-
-fn would_block(err: &io::Error) -> bool {
-    err.kind() == io::ErrorKind::WouldBlock
-}
-
-fn interrupted(err: &io::Error) -> bool {
-    err.kind() == io::ErrorKind::Interrupted
 }
