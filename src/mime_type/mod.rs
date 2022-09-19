3606fc3480f076c0d76a3ce44bac68d004c4ac3b
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-09-19 06:28:51 +0300

    wip

--- src/mime_type/mod.rs
+++ src/mime_type/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use std::ffi::OsStr;
 use std::path::Path;
 
--- src/test.rs
+++ src/test.rs
@@ -2,8 +2,6 @@
 mod range_test;
 
 #[cfg(test)]
-mod mime_type_test;
-#[cfg(test)]
 pub(crate) mod server_test;
 #[cfg(test)]
 mod thread_pool_test;
