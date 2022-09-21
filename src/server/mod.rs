b26a4763312b2d267aedc1a6819d39755d6f916d
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-09-21 21:56:15 +0300

    0.0.11 RC

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -14,10 +14,20 @@ impl Server {
     pub(crate) fn process_request(mut stream: impl Read + Write + Unpin) -> Vec<u8> {
         let mut buffer :[u8; 1024] = [0; 1024];
         stream.read(&mut buffer).unwrap();
-        let request :  &[u8] = &buffer;
-        let request: Request = Request::parse_request(request).unwrap();
+        let request : &[u8] = &buffer;
+
+
+        let boxed_request = Request::parse_request(request);
+        if boxed_request.is_err() {
+            eprintln!("unable to parse request: {}", boxed_request.err().unwrap());
+            return vec![];
+        }
+
+
+        let request: Request = boxed_request.unwrap();
         let (response, request) = App::handle_request(request);
         let raw_response = Response::generate_response(response, request);
+
         let boxed_stream = stream.write(raw_response.borrow());
         if boxed_stream.is_ok() {
             stream.flush().unwrap();
