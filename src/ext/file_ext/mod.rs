68c1cd5ed543f68b773e505331874437bb38b414
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-09-26 07:41:34 +0300

    wip

--- src/ext/file_ext/mod.rs
+++ src/ext/file_ext/mod.rs
@@ -9,6 +9,13 @@ impl FileExt {
         let mut file_content = Vec::new();
 
         let buff_length = (range.end - range.start) + 1;
+        let boxed_open = File::open(filepath);
+        if boxed_open.is_err() {
+            let error_msg = boxed_open.err().unwrap();
+            let error = format!("<p>Unable to open file: {}</p> <p>error: {}</p>", filepath, error_msg);
+            return Err(error)
+        }
+
         let file = File::open(filepath).unwrap();
         let mut reader = BufReader::new(file);
 
@@ -16,10 +23,14 @@ impl FileExt {
         if boxed_seek.is_ok() {
             let boxed_read = reader.take(buff_length).read_to_end(&mut file_content);
             if boxed_read.is_err() {
-                return Err(boxed_read.err().unwrap().to_string())
+                let error_msg = boxed_read.err().unwrap().to_string();
+                let error = format!("<p>Unable to read file: {}</p> <p>error: {}</p>", filepath, error_msg);
+                return Err(error)
             }
         } else {
-            return Err(boxed_seek.err().unwrap().to_string())
+            let error_msg = boxed_seek.err().unwrap().to_string();
+            let error = format!("<p>Unable to seek file: {}</p> <p>error: {}</p>", filepath, error_msg);
+            return Err(error)
         }
 
         Ok(file_content)
