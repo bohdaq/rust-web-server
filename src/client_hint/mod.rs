8cd5d10685697da9885dace563494f4289629c9c
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-09-27 08:07:04 +0300

    wip

--- src/client_hint/mod.rs
+++ src/client_hint/mod.rs
@@ -1,8 +1,11 @@
-pub struct AcceptClientHint {
+pub struct AskClientHint {
     pub accept_all: bool,
-    pub user_agent_cpu_architecture: bool, // Sec-CH-UA-Arch
-    pub user_agent_cpu_bitness: bool, // Sec-CH-UA-Bitness
-    pub user_agent_full_brand_information: bool, // Sec-CH-UA-Full-Version-List
-    pub user_agent_device_model: bool, // Sec-CH-UA-Model
-    pub user_agent_operating_system_version: bool, // Sec-CH-UA-Platform-Version
+    pub ask_user_agent_cpu_architecture: bool, // Sec-CH-UA-Arch
+    pub ask_user_agent_cpu_bitness: bool, // Sec-CH-UA-Bitness
+    pub ask_user_agent_full_brand_information: bool, // Sec-CH-UA-Full-Version-List
+    pub ask_user_agent_device_model: bool, // Sec-CH-UA-Model
+    pub ask_user_agent_operating_system_version: bool, // Sec-CH-UA-Platform-Version
+    pub ask_network_download_speed: bool, // Downlink (Mbps)
+    pub ask_effective_connection_type: bool, // ECT (2g/3g/4g)
+    pub ask_round_trip_time: bool, // RTT (in ms, includes server processing time)
 }
