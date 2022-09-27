003957b5e22f3edd87e7f4b1f59ea030adb03eea
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-09-27 05:40:11 +0300

    wip

--- src/client_hint/mod.rs
+++ src/client_hint/mod.rs
@@ -1,1 +1,8 @@
-pub struct ClientHint;
+pub struct AcceptClientHint {
+    pub accept_all: bool,
+    pub user_agent_cpu_architecture: bool, // Sec-CH-UA-Arch
+    pub user_agent_cpu_bitness: bool, // Sec-CH-UA-Bitness
+    pub user_agent_full_brand_information: bool, // Sec-CH-UA-Full-Version-List
+    pub user_agent_device_model: bool, // Sec-CH-UA-Model
+    pub user_agent_operating_system_version: bool, // Sec-CH-UA-Platform-Version
+}
