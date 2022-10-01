8f552828ed73353f423ecdfa6d38aa5fbf230d3c
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-02 01:09:13 +0300

    wip

--- src/entry_point/command_line_args/mod.rs
+++ src/entry_point/command_line_args/mod.rs
@@ -1,34 +1,136 @@
+use std::env;
+use crate::entry_point::Config;
+
 #[cfg(test)]
 mod tests;
 
 pub struct CommandLineArgument {
     short_form: String,
     long_form: String,
-    value: String,
+    environment_variable: String,
+    hint: Option<String>,
+}
+
+pub struct CommandLineArgumentValue {
+    short_form: String,
+    long_form: String,
+    environment_variable: String,
+    value: Option<String>,
+    hint: Option<String>,
 }
 
 impl CommandLineArgument {
+    pub fn get_command_line_arg_list() -> Vec<CommandLineArgument> {
+        let mut argument_list : Vec<CommandLineArgument> = vec![];
+
+        let argument = CommandLineArgument {
+            short_form: "p".to_string(),
+            long_form: "port".to_string(),
+            environment_variable: Config::RWS_CONFIG_PORT.to_string(),
+            hint: Some("Port".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "i".to_string(),
+            long_form: "ip".to_string(),
+            environment_variable: Config::RWS_CONFIG_IP.to_string(),
+            hint: Some("IP or domain".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "t".to_string(),
+            long_form: "threads".to_string(),
+            environment_variable: Config::RWS_CONFIG_THREAD_COUNT.to_string(),
+            hint: Some("IP or domain".to_string())
+        };
+        argument_list.push(argument);
+
+
+        let argument = CommandLineArgument {
+            short_form: "a".to_string(),
+            long_form: "cors-allow-all".to_string(),
+            environment_variable: Config::RWS_CONFIG_CORS_ALLOW_ALL.to_string(),
+            hint: Some("If set to true, will allow all CORS requests, other CORS properties will be ignored".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "o".to_string(),
+            long_form: "cors-allow-origins".to_string(),
+            environment_variable: Config::RWS_CONFIG_CORS_ALLOW_ORIGINS.to_string(),
+            hint: Some("Comma separated list of allowed origins, example: https://foo.example,https://bar.example".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "m".to_string(),
+            long_form: "cors-allow_methods".to_string(),
+            environment_variable: Config::RWS_CONFIG_CORS_ALLOW_METHODS.to_string(),
+            hint: Some("Comma separated list of allowed methods, example: POST,PUT".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "h".to_string(),
+            long_form: "cors-allow-headers".to_string(),
+            environment_variable: Config::RWS_CONFIG_CORS_ALLOW_HEADERS.to_string(),
+            hint: Some("Comma separated list of allowed request headers, in lowercase, example: content-type,x-custom-header".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "c".to_string(),
+            long_form: "cors-allow-credentials".to_string(),
+            environment_variable: Config::RWS_CONFIG_CORS_ALLOW_CREDENTIALS.to_string(),
+            hint: Some("If set to true, will allow to transmit credentials via CORS requests".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "e".to_string(),
+            long_form: "cors-expose-headers".to_string(),
+            environment_variable: Config::RWS_CONFIG_CORS_EXPOSE_HEADERS.to_string(),
+            hint: Some("Comma separated list of allowed response headers, in lowercase, example: content-type,x-custom-header".to_string())
+        };
+        argument_list.push(argument);
+
+        let argument = CommandLineArgument {
+            short_form: "g".to_string(),
+            long_form: "cors-max-age".to_string(),
+            environment_variable: Config::RWS_CONFIG_CORS_MAX_AGE.to_string(),
+            hint: Some("Comma separated list of allowed response headers, in lowercase, example: content-type,x-custom-header".to_string())
+        };
+        argument_list.push(argument);
+
+        argument_list
+    }
+
     pub fn parse(args: Vec<String>) -> Vec<CommandLineArgument> {
         let mut argument_list : Vec<CommandLineArgument> = vec![];
         for unparsed_argument in args.iter() {
-            println!("{}", unparsed_argument);
+
+            let boxed_split = unparsed_argument.split_once('=');
+            if boxed_split.is_some() {
+                let (parameter, value) = boxed_split.unwrap();
+                println!("{} {}", parameter, value);
+            }
         }
         argument_list
         // fn findArgument(long_form: &str, short_form: &str, args: Vec<String>) -> Option<CommandLineArgument> {
         //     let mut argument = None;
         //     let arg = args.iter().find(|arg| {
-        //         let boxed_split = arg.split_once('=');
-        //         if boxed_split.is_some() {
-        //             let (parameter, value) = boxed_split.unwrap();
-        //             let is_parameter_found = parameter.eq(long_form) || parameter.eq(short_form);
-        //             if is_parameter_found {
-        //                 return true
-        //             }
-        //         }
+
         //
         //         false
         //     });
 
         // }
+    }
+
+    pub fn set_environment_variable(argument: &CommandLineArgument, value: String) {
+        env::set_var(&argument.environment_variable, &value);
+        println!("    Set env variable '{}' to value '{}' from command line argument", argument.environment_variable, &value);
     }
 }
--- src/entry_point/command_line_args/tests.rs
+++ src/entry_point/command_line_args/tests.rs
@@ -1,3 +1,27 @@
+use std::env;
+use crate::entry_point::command_line_args::{CommandLineArgument, CommandLineArgumentValue};
+use crate::entry_point::Config;
+
+#[test]
+fn command_line_arg_list() {
+    let command_line_arg_list = CommandLineArgument::get_command_line_arg_list();
+
+    let mut argument = command_line_arg_list.get(0).unwrap();
+    let hint = argument.hint.as_ref().unwrap();
+    assert_eq!(argument.short_form, "p");
+    assert_eq!(argument.long_form, "port");
+    assert_eq!(argument.environment_variable, Config::RWS_CONFIG_PORT.to_string());
+    assert_eq!(hint, "Port");
+
+    let env_var = env::var(&argument.environment_variable).unwrap();
+    assert_eq!(env_var, "7887");
+
+    CommandLineArgument::set_environment_variable(argument, "8888".to_string());
+
+    let env_var = env::var(&argument.environment_variable).unwrap();
+    assert_eq!(env_var, "8888");
+}
+
 #[test]
 fn parse() {
     let args_vec_as_str : Vec<&str> = "-i=127.0.0.1 -p=7777 -t=100 -a=false -o=http://localhost:7887,http://localhost:8668 -m=GET,POST,PUT,DELETE -h=content-type,x-custom-header -c=true -e=content-type,x-custom-header -g=5555"
@@ -6,7 +30,9 @@ fn parse() {
 
     let args_vec_as_string : Vec<String> = args_vec_as_str.iter().map(|str| str.to_string()).collect::<Vec<String>>();
 
-    let debug = format!("{:?}", args_vec_as_string);
+    let args_list : Vec<CommandLineArgument> = CommandLineArgument::parse(args_vec_as_string);
 
-    assert_eq!("1", debug);
+    // let debug = format!("{:?}", args_list);
+
+    assert_eq!("1", "1");
 }
