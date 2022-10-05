9d6ffc8f2b24a9ecae6007222bb5d4d9d9360146
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-05 04:35:05 +0300

    wip

--- src/ext/date_time_ext/mod.rs
+++ src/ext/date_time_ext/mod.rs
@@ -12,17 +12,6 @@ impl DateTimeExt {
         current_utc
     }
 
-    pub fn _now_local() -> DateTime<Local> {
-        let current_local: DateTime<Local> = Local::now();
-        current_local
-    }
-
-    pub fn _now_rfc2822() -> String {
-        let current_utc: DateTime<Utc> = Utc::now();
-        let rfc2822 = current_utc.to_rfc2822();
-        rfc2822
-    }
-
     pub fn _system_time_to_utc(system_time: SystemTime) -> DateTime<Utc> {
         let seconds = system_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
         let naive_datetime = NaiveDateTime::from_timestamp(seconds as i64, 1111);
