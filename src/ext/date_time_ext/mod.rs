f1147a6114f3437fb82c954ffc358d70a90d9ae6
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-08 08:56:16 +0300

    wip

--- src/ext/date_time_ext/mod.rs
+++ src/ext/date_time_ext/mod.rs
@@ -13,7 +13,14 @@ impl DateTimeExt {
     }
 
     pub fn _system_time_to_unix_nanos(system_time: SystemTime) -> u128 {
-        let nanos = system_time.duration_since(UNIX_EPOCH).unwrap().as_nanos();
+        let boxed_duration = system_time.duration_since(UNIX_EPOCH);
+        if boxed_duration.is_err() {
+            eprintln!("unable to get duration from system time {}", boxed_duration.err().unwrap());
+            let nanos = 0 as u128;
+            return nanos
+        }
+
+        let nanos = boxed_duration.unwrap().as_nanos();
         nanos
     }
 
