23fa555f98a7efa833cbc30e88f10fb6189e9b1f
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-08 09:14:06 +0200

    wip

--- src/entry_point/mod.rs
+++ src/entry_point/mod.rs
@@ -60,8 +60,13 @@ pub fn bootstrap() {
 pub fn set_default_values() {
     println!("  Initializing default values");
 
-    env::set_var(Config::RWS_CONFIG_IP, Config::RWS_CONFIG_IP_DEFAULT_VALUE);
-    println!("    Default value  for '{}' is '{}'", Config::RWS_CONFIG_IP, Config::RWS_CONFIG_IP_DEFAULT_VALUE);
+    let is_var_set = env::var(Config::RWS_CONFIG_IP).is_ok();
+    if !is_var_set {
+        env::set_var(Config::RWS_CONFIG_IP, Config::RWS_CONFIG_IP_DEFAULT_VALUE);
+        println!("    Default value  for '{}' is '{}'", Config::RWS_CONFIG_IP, Config::RWS_CONFIG_IP_DEFAULT_VALUE);
+    } else {
+        println!("    There is an environment variable  for '{}', omitting default value set", Config::RWS_CONFIG_IP);
+    }
 
     env::set_var(Config::RWS_CONFIG_PORT, Config::RWS_CONFIG_PORT_DEFAULT_VALUE);
     println!("    Default value  for '{}' is '{}'", Config::RWS_CONFIG_PORT, Config::RWS_CONFIG_PORT_DEFAULT_VALUE);
