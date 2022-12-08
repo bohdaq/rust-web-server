f473f4ac439df23e71d3f05a67e1720e5992dbdd
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-08 18:31:06 +0200

    wip

--- src/app/controller/style/mod.rs
+++ src/app/controller/style/mod.rs
@@ -3,15 +3,14 @@ use crate::mime_type::MimeType;
 use crate::range::Range;
 use crate::request::Request;
 use crate::response::{Response, STATUS_CODE_REASON_PHRASE};
-use crate::symbol::SYMBOL;
 
-pub struct IndexController;
+pub struct StyleController;
 
-impl IndexController {
-    pub const INDEX_FILEPATH: &'static str = "index.html";
+impl StyleController {
+    pub const STYLE_FILEPATH: &'static str = "style.css";
 
     pub fn is_matching_request(request: &Request) -> bool {
-        request.request_uri == SYMBOL.slash
+        request.request_uri == "/style.css"
     }
 
     pub fn process_request(_request: &Request, mut response: Response) -> Response {
@@ -19,9 +18,9 @@ impl IndexController {
         response.reason_phrase = STATUS_CODE_REASON_PHRASE.n200_ok.reason_phrase.to_string();
 
 
-        if FileExt::does_file_exist(IndexController::INDEX_FILEPATH) {
+        if FileExt::does_file_exist(StyleController::STYLE_FILEPATH) {
             let boxed_content_range =
-                Range::get_content_range_of_a_file(IndexController::INDEX_FILEPATH);
+                Range::get_content_range_of_a_file(StyleController::STYLE_FILEPATH);
 
             if boxed_content_range.is_ok() {
                 let content_range = boxed_content_range.unwrap();
@@ -41,10 +40,10 @@ impl IndexController {
                 response.reason_phrase = STATUS_CODE_REASON_PHRASE.n500_internal_server_error.reason_phrase.to_string();
             }
         } else {
-            let index_file = include_bytes!("index.html");
+            let style_file = include_bytes!("style.css");
 
             let content_range =
-                Range::get_content_range(index_file.to_vec(), MimeType::TEXT_HTML.to_string());
+                Range::get_content_range(style_file.to_vec(), MimeType::TEXT_CSS.to_string());
 
 
             let content_range_list = vec![content_range];
