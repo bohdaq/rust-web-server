43b3a7c707e217753f6b3951a295d995aca8294c
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-26 22:19:33 +0200

    added test on symlink

--- src/server/tests.rs
+++ src/server/tests.rs
@@ -109,6 +109,70 @@ fn it_generates_successful_response_with_index_html() {
 }
 
 #[test]
+fn it_generates_successful_response_with_index_html_as_symlink() {
+    // request test data
+    let request_host_header_name = "Host";
+    let request_host_header_value = "localhost:7777";
+    let request_method = METHOD.get;
+    let request_uri = [SYMBOL.slash, "index_rewrite"].join("");
+    let request_http_version = VERSION.http_1_1.to_string();
+
+
+    // request part
+    let host = Header {
+        name: request_host_header_name.to_string(),
+        value: request_host_header_value.to_string()
+    };
+
+    let headers = vec![host];
+    let request = Request {
+        method: request_method.to_string(),
+        request_uri: request_uri.to_string(),
+        http_version: request_http_version.to_string(),
+        headers
+    };
+
+    let raw_request = Request::_generate_request(request);
+
+    let request: Request = Request::parse_request(&raw_request.as_bytes()).unwrap();
+    let host_header = request.get_header(request_host_header_name.to_string()).unwrap();
+
+    assert_eq!(request_host_header_value.to_string(), host_header.value);
+    assert_eq!(request_method.to_string(), request.method);
+    assert_eq!(request_uri.to_string(), request.request_uri);
+    assert_eq!(request_http_version.to_string(), request.http_version);
+
+    // response part
+    let response_http_version = VERSION.http_1_1.to_string();
+    let response_status_code = STATUS_CODE_REASON_PHRASE.n200_ok.status_code;
+    let response_reason_phrase = STATUS_CODE_REASON_PHRASE.n200_ok.reason_phrase;
+    let response_filepath = "index.html";
+    let response_html_file= fs::read_to_string(response_filepath.to_string()).unwrap();
+    let response_content_length_header_name = "Content-Length";
+    let response_content_length_header_value = response_html_file.len().to_string();
+
+    let mock_tcp_stream = MockTcpStream {
+        read_data: raw_request.as_bytes().to_vec(),
+        write_data: vec![],
+    };
+    let peer_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0,0,0,0)), 0);
+    let raw_response: Vec<u8> = Server::process_request(mock_tcp_stream, peer_addr);
+    let response = Response::_parse_response(raw_response.borrow());
+    let header = response._get_header(response_content_length_header_name.to_string()).unwrap();
+
+    let content_type_header = response._get_header(Header::_CONTENT_TYPE.to_string()).unwrap();
+    let x_content_type_options_header = response._get_header(Header::_X_CONTENT_TYPE_OPTIONS.to_string()).unwrap();
+
+    assert_eq!(Header::_X_CONTENT_TYPE_OPTIONS_VALUE_NOSNIFF, x_content_type_options_header.value);
+    assert_eq!(MimeType::TEXT_HTML, content_type_header.value);
+    assert_eq!(response_content_length_header_value, header.value);
+    assert_eq!(response_http_version, response.http_version);
+    assert_eq!(*response_status_code, response.status_code);
+    assert_eq!(response_reason_phrase, response.reason_phrase);
+    assert_eq!(response_html_file.as_bytes().to_vec(), response.content_range_list.get(0).unwrap().body);
+}
+
+#[test]
 fn it_generates_successful_response_with_static_file() {
     override_environment_variables_from_config(Some("/src/test/rws.config.toml"));
 
