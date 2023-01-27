f7e8f9392cc5a8ffb5a8b7b23adf75c15e41c729
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-01-27 12:50:29 +0200

    wip

--- src/experimental/request/tests.rs
+++ src/experimental/request/tests.rs
@@ -275,25 +275,27 @@ fn file_upload_multipart_form_data_content_type() {
         new_line.to_string(),
     ].join(SYMBOL.empty_string);
 
-    let path = FileExt::build_path(&["src", "test", "rws.config.toml"]);
+    let filename = "rws.config.toml";
+    let path = FileExt::build_path(&["src", "test", filename]);
     let boxed_payload = FileExt::read_file(&path);
     assert!(boxed_payload.is_ok());
 
     let payload = boxed_payload.unwrap();
-    // let key = "key";
-    // let payload_boundary = format!("{}{}", boundary,  SYMBOL.new_line_carriage_return);
-    // let content_disposition = format!("Content-Disposition: form-data; name=\"{}\"{}", key, SYMBOL.new_line_carriage_return);
-    // let raw_payload_2 = [
-    //     payload_boundary,
-    //     content_disposition,
-    //     new_line.to_string(),
-    //     payload,
-    //     new_line.to_string(),
-    // ].join(SYMBOL.empty_string);
+    let key = "fileupload";
+    let payload_boundary = format!("{}{}", boundary,  SYMBOL.new_line_carriage_return);
+    let content_disposition = format!("Content-Disposition: form-data; name=\"{}\"{}; filename=\"{}\"", key, filename, SYMBOL.new_line_carriage_return);
+    let raw_payload_3 = [
+         payload_boundary,
+         content_disposition,
+         new_line.to_string(),
+         String::from_utf8(payload).unwrap(), // payload is not escaped, text file used for test
+         new_line.to_string(),
+     ].join(SYMBOL.empty_string);
 
     let raw_payload = [
         raw_payload_1,
         raw_payload_2,
+        raw_payload_3,
         boundary.to_string(),
     ].join(SYMBOL.empty_string);
 
