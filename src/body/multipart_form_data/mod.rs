fd0a0dcb2b2a5e5d8f248925322a61f87c94df89
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-02-01 11:40:53 +0200

    wip

--- src/body/multipart_form_data/mod.rs
+++ src/body/multipart_form_data/mod.rs
@@ -1,5 +1,15 @@
+use crate::header::Header;
+
 pub struct FormMultipartData;
 
-impl FormMultipartData {
+pub struct Part {
+    pub headers: Vec<Header>,
+    pub body: Vec<u8>,
+}
 
+impl FormMultipartData {
+    pub fn parse(data: Vec<u8>) -> Result<Vec<Part>, String> {
+        let parts = vec![];
+        Ok(parts)
+    }
 }
