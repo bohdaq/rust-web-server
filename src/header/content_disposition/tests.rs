431ed4e5ab8378edee25da1dc4b8aa5a60d754fb
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-02-05 13:58:00 +0200

    wip

--- src/header/content_disposition/tests.rs
+++ src/header/content_disposition/tests.rs
@@ -6,3 +6,10 @@ fn inline() {
     let content_disposition = ContentDisposition::parse(raw_content_disposition).unwrap();
     assert_eq!(content_disposition.disposition_type, raw_content_disposition);
 }
+
+#[test]
+fn attachment() {
+    let raw_content_disposition = "attachment";
+    let content_disposition = ContentDisposition::parse(raw_content_disposition).unwrap();
+    assert_eq!(content_disposition.disposition_type, raw_content_disposition);
+}
