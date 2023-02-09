b7fec26d5dd1f47ca7240854a8b735cc46b3c11b
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-02-09 18:48:41 +0200

    wip

--- src/app/controller/form/multipart_enctype_post_method/mod.rs
+++ src/app/controller/form/multipart_enctype_post_method/mod.rs
@@ -16,7 +16,10 @@ impl FormMultipartEnctypePostMethodController {
 
     pub fn is_matching_request(request: &Request) -> bool {
         let boxed_content_type = request.get_header(Header::_CONTENT_TYPE.to_string());
-        if boxed_content_type.is_none() { return false; }
+        if boxed_content_type.is_none() {
+            eprintln!("unable to get content-type header for {} {}", request.method, request.request_uri);
+            return false;
+        }
 
         let boxed_path = request.get_uri_path();
         if boxed_path.is_err() {
@@ -42,10 +45,31 @@ impl FormMultipartEnctypePostMethodController {
 
 
         // here is the list of parts, as an example here it is printed in the response body
-        // TODO: error-handling
+
+        // content-type is already checked in is_matching_request, no additional checks
         let content_type = _request.get_header(Header::_CONTENT_TYPE.to_string()).unwrap();
-        let boundary = FormMultipartData::extract_boundary(&content_type.value).unwrap();
-        let part_list = FormMultipartData::parse(&_request.body.clone(), boundary).unwrap();
+
+        let boxed_boundary = FormMultipartData::extract_boundary(&content_type.value);
+        if boxed_boundary.is_err() {
+            response.status_code = *STATUS_CODE_REASON_PHRASE.n400_bad_request.status_code;
+            response.reason_phrase = STATUS_CODE_REASON_PHRASE.n400_bad_request.reason_phrase.to_string();
+            let message = boxed_boundary.err().unwrap();
+            response.content_range_list = vec![
+                ContentRange{
+                    unit: Range::BYTES.to_string(),
+                    range: Range { start: 0, end: message.len() as u64 },
+                    size: message.len().to_string(),
+                    body: Vec::from(message.as_bytes()),
+                    content_type: MimeType::TEXT_PLAIN.to_string(),
+                }
+            ];
+            return response;
+        }
+        let boundary = boxed_boundary.unwrap();
+
+        // TODO: error-handling
+        let boxed_part_list = FormMultipartData::parse(&_request.body.clone(), boundary);
+        let part_list = boxed_part_list.unwrap();
 
         let mut formatted_list : Vec<String> = vec![];
         for part in part_list.into_iter() {
