9f4145f14c4cf4a55d2fd53457e00ba152d3c883
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-09 17:05:26 +0200

    wip

--- src/json/tests.rs
+++ src/json/tests.rs
@@ -2240,3 +2240,28 @@ fn json_array_whitespace_after_last_element() {
     let expected = vec!["123", "456", "6", "7", "8"];
     assert_eq!(actual, expected);
 }
+
+#[test]
+fn json_array_whitespace_before_array() {
+    let array = " [ 123, 456, 6,7 ,8 ]";
+    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let expected = vec!["123", "456", "6", "7", "8"];
+    assert_eq!(actual, expected);
+}
+
+
+#[test]
+fn json_array_whitespace_after_array() {
+    let array = " [ 123, 456, 6,7 ,8 ] ";
+    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let expected = vec!["123", "456", "6", "7", "8"];
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn json_empty_array() {
+    let array = " [  ] ";
+    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let expected : Vec<String> = vec![];
+    assert_eq!(actual, expected);
+}
