23e2871fe996e3303ac2de42b6c215532e2c7582
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-11 23:09:09 +0200

    wip

--- src/json/tests.rs
+++ src/json/tests.rs
@@ -2259,9 +2259,9 @@ fn json_array_wrong_element() {
 
 #[test]
 fn json_array_whitespace_before_first_element() {
-    let array = "[ 123, -456, 0,7 ,8]";
+    let array = "[ 123.76, -456, 0,7.5e4 ,8]";
     let actual = JSONArray::parse(array.to_string()).unwrap();
-    let expected = vec!["123", "-456", "0", "7", "8"];
+    let expected = vec!["123.76", "-456", "0", "7.5e4", "8"];
     assert_eq!(actual, expected);
 }
 
