6598f14e355db239215b9f8ff4c2badebff0c3c3
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-11 12:39:37 +0200

    wip

--- src/json/tests.rs
+++ src/json/tests.rs
@@ -2242,6 +2242,14 @@ fn json_array_strings() {
 }
 
 #[test]
+fn json_array_strings_multichar() {
+    let array = "[\"ab\", \"bb\", \"bc\",\"db\" ,\"eb\"]";
+    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let expected = vec!["\"ab\"", "\"bb\"", "\"bc\"", "\"db\"", "\"eb\""];
+    assert_eq!(actual, expected);
+}
+
+#[test]
 fn json_array_whitespace_before_first_element() {
     let array = "[ 123, 456, 6,7 ,8]";
     let actual = JSONArray::parse(array.to_string()).unwrap();
