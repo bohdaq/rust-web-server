6337a05a797819ef5dff4bc79ee28b2272fb35f1
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-16 23:04:05 +0200

    wip

--- src/json/array/mod.rs
+++ src/json/array/mod.rs
@@ -1,15 +1,34 @@
 use std::io;
 use std::io::Read;
+use crate::json::object::ToJSON;
 use crate::symbol::SYMBOL;
 
 #[cfg(test)]
 mod tests;
 
-pub struct JSONArray {
-
+pub struct JSONArray<T> {
+    _items: T, // added to eliminate compiler error
 }
 
-impl JSONArray {
+impl<T: ToJSON> JSONArray<T> {
+    pub fn to_json(items : Vec<T>) -> Result<String, String> {
+        let mut json_vec = vec![];
+        json_vec.push(SYMBOL.opening_square_bracket.to_string());
+        for (pos, item) in items.iter().enumerate() {
+            json_vec.push(item.to_json_string());
+            if pos != items.len() - 1 {
+                json_vec.push(SYMBOL.comma.to_string());
+                json_vec.push(SYMBOL.new_line_carriage_return.to_string());
+            }
+        }
+        json_vec.push(SYMBOL.closing_square_bracket.to_string());
+
+        let result = json_vec.join(SYMBOL.empty_string);
+        Ok(result)
+    }
+}
+
+impl<T> JSONArray<T> {
     pub fn parse(_json_string: String) -> Result<Vec<String>, String> {
         let mut list : Vec<String> = vec![];
 
--- src/json/array/tests.rs
+++ src/json/array/tests.rs
@@ -5,9 +5,122 @@ use crate::json::property::JSONProperty;
 use crate::symbol::SYMBOL;
 
 #[test]
+fn vector_to_json() {
+    struct SomeObject {
+        prop_a: String,
+        prop_b: bool
+    }
+
+    impl FromJSON for SomeObject {
+        fn parse_json_to_properties(&self, json_string: String) -> Result<Vec<(JSONProperty, JSONValue)>, String> {
+            let boxed_parse = JSON::parse_as_properties(json_string);
+            if boxed_parse.is_err() {
+                let message = boxed_parse.err().unwrap();
+                return Err(message)
+            }
+            let properties = boxed_parse.unwrap();
+            Ok(properties)
+        }
+        fn set_properties(&mut self, properties: Vec<(JSONProperty, JSONValue)>) -> Result<(), String> {
+            for (property, value) in properties {
+                if property.property_name == "prop_a" {
+                    self.prop_a = value.string.unwrap();
+                }
+                if property.property_name == "prop_b" {
+                    self.prop_b = value.bool.unwrap();
+                }
+            }
+            Ok(())
+        }
+        fn parse(&mut self, json_string: String) -> Result<(), String> {
+            let boxed_properties = self.parse_json_to_properties(json_string);
+            if boxed_properties.is_err() {
+                let message = boxed_properties.err().unwrap();
+                return Err(message);
+            }
+            let properties = boxed_properties.unwrap();
+            let boxed_set = self.set_properties(properties);
+            if boxed_set.is_err() {
+                let message = boxed_set.err().unwrap();
+                return Err(message);
+            }
+            Ok(())
+        }
+    }
+
+    impl ToJSON for SomeObject {
+        fn list_properties() -> Vec<JSONProperty> {
+            let mut list = vec![];
+
+            let property = JSONProperty { property_name: "prop_a".to_string(), property_type: JSON_TYPE.string.to_string() };
+            list.push(property);
+
+            let property = JSONProperty { property_name: "prop_b".to_string(), property_type: JSON_TYPE.boolean.to_string() };
+            list.push(property);
+
+            list
+        }
+
+        fn get_property(&self, property_name: String) -> JSONValue {
+            let mut value = JSONValue::new();
+
+            if property_name == "prop_a".to_string() {
+                let string : String = self.prop_a.to_owned();
+                value.string = Some(string);
+            }
+
+            if property_name == "prop_b".to_string() {
+                let boolean : bool = self.prop_b;
+                value.bool = Some(boolean);
+            }
+
+            value
+        }
+
+        fn to_json_string(&self) -> String {
+            let mut json_list = vec![];
+            json_list.push(SYMBOL.opening_curly_bracket.to_string());
+
+
+            let mut properties_list = vec![];
+
+            let properties = SomeObject::list_properties();
+            for property in properties {
+                let value = self.get_property(property.property_name.to_string());
+
+                if &property.property_type == "String" {
+                    let raw_value = value.string.unwrap();
+                    let formatted_property = format!("  \"{}\": \"{}\"", &property.property_name, raw_value);
+                    properties_list.push(formatted_property.to_string());
+                }
+
+                if &property.property_type == "bool" {
+                    let raw_value = value.bool.unwrap();
+                    let formatted_property = format!("  \"{}\": {}", &property.property_name, raw_value);
+                    properties_list.push(formatted_property.to_string());
+                }
+            }
+
+
+            let comma_new_line_carriage_return = format!("{}{}", SYMBOL.comma, SYMBOL.new_line_carriage_return);
+            let properties = properties_list.join(&comma_new_line_carriage_return);
+
+            json_list.push(properties);
+            json_list.push(SYMBOL.closing_curly_bracket.to_string());
+            let json= json_list.join(SYMBOL.new_line_carriage_return);
+            json
+        }
+    }
+
+    let obj = SomeObject { prop_a: "default".to_string(), prop_b: false };
+    let actual = JSONArray::<SomeObject>::to_json(vec![obj]).unwrap();
+    assert_eq!(actual, "[{\r\n  \"prop_a\": \"default\",\r\n  \"prop_b\": false\r\n}]".to_string());
+}
+
+#[test]
 fn json_array_true_element() {
     let array = "[true]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["true"];
     assert_eq!(actual, expected);
 }
@@ -15,7 +128,7 @@ fn json_array_true_element() {
 #[test]
 fn json_array_false_element() {
     let array = "[false]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["false"];
     assert_eq!(actual, expected);
 }
@@ -23,7 +136,7 @@ fn json_array_false_element() {
 #[test]
 fn json_array_nested_array() {
     let array = "[ [false] ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["[false]"];
     assert_eq!(actual, expected);
 }
@@ -31,7 +144,7 @@ fn json_array_nested_array() {
 #[test]
 fn json_array_nested_object() {
     let array = "[ {\"prop_b\": true, \"prop_a\": \"123abc\"} ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["{\"prop_b\": true, \"prop_a\": \"123abc\"}"];
     assert_eq!(actual, expected);
 
@@ -156,7 +269,7 @@ fn json_array_nested_object() {
 #[test]
 fn json_array_nested_empty_object() {
     let array = "[ {} ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["{}"];
     assert_eq!(actual, expected);
 }
@@ -164,7 +277,7 @@ fn json_array_nested_empty_object() {
 #[test]
 fn json_array_nested_object_nested_array() {
     let array = "[ {\"key\": [123, 456, 789, 10]} ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["{\"key\": [123, 456, 789, 10]}"];
     assert_eq!(actual, expected);
 }
@@ -172,7 +285,7 @@ fn json_array_nested_object_nested_array() {
 #[test]
 fn json_array_nested_array_multiple_items() {
     let array = "[ [true,0, null, -1, 2.0, \"text\", false] ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["[true,0, null, -1, 2.0, \"text\", false]"];
     assert_eq!(actual, expected);
 }
@@ -180,7 +293,7 @@ fn json_array_nested_array_multiple_items() {
 #[test]
 fn json_array_multi_nested_array_multiple_items() {
     let array = "[ [true,0, [null, -1], 2.0, \"text\", false] ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["[true,0, [null, -1], 2.0, \"text\", false]"];
     assert_eq!(actual, expected);
 }
@@ -188,7 +301,7 @@ fn json_array_multi_nested_array_multiple_items() {
 #[test]
 fn json_array_multiple_true_elements() {
     let array = "[ true,true]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["true", "true"];
     assert_eq!(actual, expected);
 }
@@ -196,7 +309,7 @@ fn json_array_multiple_true_elements() {
 #[test]
 fn json_array_multiple_false_elements() {
     let array = "[false , false]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["false", "false"];
     assert_eq!(actual, expected);
 }
@@ -204,7 +317,7 @@ fn json_array_multiple_false_elements() {
 #[test]
 fn json_array_null_element() {
     let array = "[null]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["null"];
     assert_eq!(actual, expected);
 }
@@ -212,7 +325,7 @@ fn json_array_null_element() {
 #[test]
 fn json_array_multiple_null_elements() {
     let array = "[null ,null]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["null", "null"];
     assert_eq!(actual, expected);
 }
@@ -220,7 +333,7 @@ fn json_array_multiple_null_elements() {
 #[test]
 fn json_array_multiple_elements() {
     let array = "[true,0, null, -1, 2.0, \"text\", false]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["true", "0", "null", "-1", "2.0", "\"text\"", "false"];
     assert_eq!(actual, expected);
 }
@@ -228,7 +341,7 @@ fn json_array_multiple_elements() {
 #[test]
 fn json_array() {
     let array = "[123, 456, 6,7 ,8]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["123", "456", "6", "7", "8"];
     assert_eq!(actual, expected);
 }
@@ -236,7 +349,7 @@ fn json_array() {
 #[test]
 fn json_array_float() {
     let array = "[123.123, 456.456, 6.534e123,7 ,8.0]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["123.123", "456.456", "6.534e123", "7", "8.0"];
     assert_eq!(actual, expected);
 }
@@ -244,7 +357,7 @@ fn json_array_float() {
 #[test]
 fn json_array_strings() {
     let array = "[\"a\", \"b\", \"c\",\"d\" ,\"e\"]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["\"a\"", "\"b\"", "\"c\"", "\"d\"", "\"e\""];
     assert_eq!(actual, expected);
 }
@@ -252,7 +365,7 @@ fn json_array_strings() {
 #[test]
 fn json_array_strings_multichar() {
     let array = "[\"ab\", \"bb\", \"bc\",\"db\" ,\"eb\"]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["\"ab\"", "\"bb\"", "\"bc\"", "\"db\"", "\"eb\""];
     assert_eq!(actual, expected);
 }
@@ -260,7 +373,7 @@ fn json_array_strings_multichar() {
 #[test]
 fn json_array_wrong_element() {
     let array = "[ asdfg, 456, 6,7 ,8]";
-    let actual = JSONArray::parse(array.to_string()).err().unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).err().unwrap();
     let expected = "unknown type: a in [ asdfg, 456, 6,7 ,8]";
     assert_eq!(actual, expected);
 }
@@ -268,7 +381,7 @@ fn json_array_wrong_element() {
 #[test]
 fn json_array_wrong_element_duplicate_minus() {
     let array = "[ --35346, 456, 6,7 ,8]";
-    let actual = JSONArray::parse(array.to_string()).err().unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).err().unwrap();
     let expected = "unable to parse number: - in [ --35346, 456, 6,7 ,8]";
     assert_eq!(actual, expected);
 }
@@ -276,7 +389,7 @@ fn json_array_wrong_element_duplicate_minus() {
 #[test]
 fn json_array_wrong_element_duplicate_exponent() {
     let array = "[ 6e2e2]";
-    let actual = JSONArray::parse(array.to_string()).err().unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).err().unwrap();
     let expected = "unable to parse number: 6e2 in [ 6e2e2]";
     assert_eq!(actual, expected);
 }
@@ -284,7 +397,7 @@ fn json_array_wrong_element_duplicate_exponent() {
 #[test]
 fn json_array_wrong_element_duplicate_point() {
     let array = "[ 6.2.2]";
-    let actual = JSONArray::parse(array.to_string()).err().unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).err().unwrap();
     let expected = "unable to parse number: 6.2 in [ 6.2.2]";
     assert_eq!(actual, expected);
 }
@@ -292,7 +405,7 @@ fn json_array_wrong_element_duplicate_point() {
 #[test]
 fn json_array_wrong_element_rundom_char() {
     let array = "[ 6h2]";
-    let actual = JSONArray::parse(array.to_string()).err().unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).err().unwrap();
     let expected = "unable to parse number: h in [ 6h2]";
     assert_eq!(actual, expected);
 }
@@ -300,7 +413,7 @@ fn json_array_wrong_element_rundom_char() {
 #[test]
 fn json_array_whitespace_before_first_element() {
     let array = "[ 123.76, -456, 0,7.5e4 ,8]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["123.76", "-456", "0", "7.5e4", "8"];
     assert_eq!(actual, expected);
 }
@@ -308,7 +421,7 @@ fn json_array_whitespace_before_first_element() {
 #[test]
 fn json_array_whitespace_after_last_element() {
     let array = "[ 123, 456, 6,7 ,8 ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["123", "456", "6", "7", "8"];
     assert_eq!(actual, expected);
 }
@@ -316,7 +429,7 @@ fn json_array_whitespace_after_last_element() {
 #[test]
 fn json_array_whitespace_before_array() {
     let array = " [ 123, 456, 6,7 ,8 ]";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["123", "456", "6", "7", "8"];
     assert_eq!(actual, expected);
 }
@@ -325,7 +438,7 @@ fn json_array_whitespace_before_array() {
 #[test]
 fn json_array_whitespace_after_array() {
     let array = " [ 123, 456, 6,7 ,8 ] ";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected = vec!["123", "456", "6", "7", "8"];
     assert_eq!(actual, expected);
 }
@@ -333,7 +446,7 @@ fn json_array_whitespace_after_array() {
 #[test]
 fn json_empty_array() {
     let array = " [  ] ";
-    let actual = JSONArray::parse(array.to_string()).unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).unwrap();
     let expected : Vec<String> = vec![];
     assert_eq!(actual, expected);
 }
@@ -341,7 +454,7 @@ fn json_empty_array() {
 #[test]
 fn json_array_starts_with_random_chars() {
     let array = "adgsfdg [ 123, 456, 6,7 ,8 ] ";
-    let actual = JSONArray::parse(array.to_string()).err().unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).err().unwrap();
     let expected = "input string does not start with opening square bracket: a in adgsfdg [ 123, 456, 6,7 ,8 ] ";
     assert_eq!(actual, expected);
 }
@@ -349,7 +462,7 @@ fn json_array_starts_with_random_chars() {
 #[test]
 fn json_array_ends_with_random_chars() {
     let array = " [ 123, 456, 6,7 ,8 ] adgsfdg";
-    let actual = JSONArray::parse(array.to_string()).err().unwrap();
+    let actual = JSONArray::<String>::parse(array.to_string()).err().unwrap();
     let expected = "after array there are some characters: a in  [ 123, 456, 6,7 ,8 ] adgsfdg";
     assert_eq!(actual, expected);
 }
@@ -357,7 +470,7 @@ fn json_array_ends_with_random_chars() {
 #[test]
 fn json_array_no_closing_square_bracket() {
     let array = " [ 123, 456, 6,7 ,8  ";
-    let result = JSONArray::parse(array.to_string());
+    let result = JSONArray::<String>::parse(array.to_string());
     assert!(result.is_err());
 
     let message = result.err().unwrap();
@@ -367,7 +480,7 @@ fn json_array_no_closing_square_bracket() {
 #[test]
 fn json_array_no_starting_square_bracket() {
     let array = "  123, 456, 6,7 ,8  ]";
-    let result = JSONArray::parse(array.to_string());
+    let result = JSONArray::<String>::parse(array.to_string());
     assert!(result.is_err());
 
     let message = result.err().unwrap();
@@ -377,7 +490,7 @@ fn json_array_no_starting_square_bracket() {
 #[test]
 fn json_array_whitespaces() {
     let array = "  ";
-    let result = JSONArray::parse(array.to_string());
+    let result = JSONArray::<String>::parse(array.to_string());
     assert!(result.is_err());
 
     let message = result.err().unwrap();
@@ -387,7 +500,7 @@ fn json_array_whitespaces() {
 #[test]
 fn json_array_missing_comma() {
     let array = "[  123, 456 6,7 ,8  ]";
-    let result = JSONArray::parse(array.to_string());
+    let result = JSONArray::<String>::parse(array.to_string());
     assert!(result.is_err());
 
     let message = result.err().unwrap();
