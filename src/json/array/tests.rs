dfd12f0229f3812b2bcd87bbeac9dbdb7eec910c
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-17 03:10:20 +0200

    wip

--- src/json/array/tests.rs
+++ src/json/array/tests.rs
@@ -143,7 +143,6 @@ fn vector_to_json() {
 #[test]
 fn json_array_true_element() {
     let array = "[true]";
-    //TODO: JSONArrayOfPrimitives::parse(array.to_string()).unwrap();
     let actual = RawUnprocessedJSONArray::split_into_vector_of_strings(array.to_string()).unwrap();
     let expected = vec!["true"];
     assert_eq!(actual, expected);
