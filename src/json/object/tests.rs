69175f0ab5c9caad4c1c5841d1780254635001d1
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-17 18:02:54 +0200

    wip

--- src/json/object/tests.rs
+++ src/json/object/tests.rs
@@ -173,7 +173,7 @@ fn parse_direct() {
 
 #[test]
 fn parse_new_lines_carriage_returns() {
-    let json_string = "{\r\n  \r\n\"prop_a\"\r\n:\r\n \"123abc\",\r\n  \"prop_b\": true,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 4356257,\r\n  \"prop_e\": 4356.257\r\n}";
+    let json_string = "{\r\n  \r\n\"prop_a\"\r\n:\r\n \r\n\"123abc\"\r\n,\r\n  \"prop_b\": true,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 4356257,\r\n  \"prop_e\": 4356.257\r\n}";
 
     let mut deserealized_object = ExampleObject {
         prop_a: "".to_string(),
