5bc8348e9ee75afd5fb2e59e7b6827bb93eff6a8
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-17 17:51:27 +0200

    wip

--- src/json/object/tests.rs
+++ src/json/object/tests.rs
@@ -1,7 +1,10 @@
 use crate::json::{JSONValue, JSON_TYPE};
 use crate::json::property::JSONProperty;
 use crate::json::object::{FromJSON, JSON, ToJSON};
+use crate::json::object::tests::example_object::ExampleObject;
 use crate::symbol::SYMBOL;
+
+mod example_object;
 
 #[test]
 fn parse() {
@@ -137,134 +140,9 @@ fn parse() {
     assert_eq!(true, obj.prop_b);
 }
 
-
 #[test]
 fn parse_direct() {
-    struct SomeObject {
-        prop_a: String,
-        prop_b: bool,
-        prop_c: bool,
-        prop_d: i128,
-        prop_e: f64
-    }
-
-    impl FromJSON for SomeObject {
-        fn parse_json_to_properties(&self, json_string: String) -> Result<Vec<(JSONProperty, JSONValue)>, String> {
-            let boxed_parse = JSON::parse_as_properties(json_string);
-            if boxed_parse.is_err() {
-                let message = boxed_parse.err().unwrap();
-                return Err(message)
-            }
-            let properties = boxed_parse.unwrap();
-            Ok(properties)
-        }
-        fn set_properties(&mut self, properties: Vec<(JSONProperty, JSONValue)>) -> Result<(), String> {
-            for (property, value) in properties {
-                if property.property_name == "prop_a" {
-                    self.prop_a = value.string.unwrap();
-                }
-                if property.property_name == "prop_b" {
-                    self.prop_b = value.bool.unwrap();
-                }
-
-                if property.property_name == "prop_c" {
-                    self.prop_c = value.bool.unwrap();
-                }
-
-                if property.property_name == "prop_d" {
-                    self.prop_d = value.i128.unwrap();
-                }
-
-                if property.property_name == "prop_e" {
-                    self.prop_e = value.f64.unwrap();
-                }
-            }
-            Ok(())
-        }
-        fn parse(&mut self, json_string: String) -> Result<(), String> {
-            let boxed_properties = self.parse_json_to_properties(json_string);
-            if boxed_properties.is_err() {
-                let message = boxed_properties.err().unwrap();
-                return Err(message);
-            }
-            let properties = boxed_properties.unwrap();
-            let boxed_set = self.set_properties(properties);
-            if boxed_set.is_err() {
-                let message = boxed_set.err().unwrap();
-                return Err(message);
-            }
-            Ok(())
-        }
-    }
-
-    impl ToJSON for SomeObject {
-        fn list_properties() -> Vec<JSONProperty> {
-            let mut list = vec![];
-
-            let property = JSONProperty { property_name: "prop_a".to_string(), property_type: JSON_TYPE.string.to_string() };
-            list.push(property);
-
-            let property = JSONProperty { property_name: "prop_b".to_string(), property_type: JSON_TYPE.boolean.to_string() };
-            list.push(property);
-
-            let property = JSONProperty { property_name: "prop_c".to_string(), property_type: JSON_TYPE.boolean.to_string() };
-            list.push(property);
-
-            let property = JSONProperty { property_name: "prop_d".to_string(), property_type: JSON_TYPE.integer.to_string() };
-            list.push(property);
-
-            let property = JSONProperty { property_name: "prop_e".to_string(), property_type: JSON_TYPE.number.to_string() };
-            list.push(property);
-
-            list
-        }
-
-        fn get_property(&self, property_name: String) -> JSONValue {
-            let mut value = JSONValue::new();
-
-            if property_name == "prop_a".to_string() {
-                let string : String = self.prop_a.to_owned();
-                value.string = Some(string);
-            }
-
-            if property_name == "prop_b".to_string() {
-                let boolean : bool = self.prop_b;
-                value.bool = Some(boolean);
-            }
-
-            if property_name == "prop_c".to_string() {
-                let boolean : bool = self.prop_c;
-                value.bool = Some(boolean);
-            }
-
-            if property_name == "prop_d".to_string() {
-                let integer : i128 = self.prop_d;
-                value.i128 = Some(integer);
-            }
-
-            if property_name == "prop_e".to_string() {
-                let floating_point_number: f64 = self.prop_e;
-                value.f64 = Some(floating_point_number);
-            }
-
-            value
-        }
-
-        fn to_json_string(&self) -> String {
-            let mut processed_data = vec![];
-
-            let properties = SomeObject::list_properties();
-            for property in properties {
-                let value = self.get_property(property.property_name.to_string());
-                processed_data.push((property, value));
-
-            }
-
-            JSON::to_json_string(processed_data)
-        }
-    }
-
-    let obj = SomeObject {
+    let obj = ExampleObject {
         prop_a: "123abc".to_string(),
         prop_b: true,
         prop_c: false,
@@ -277,7 +155,7 @@ fn parse_direct() {
 
     assert_eq!(expected_json_string, json_string);
 
-    let mut deserealized_object = SomeObject {
+    let mut deserealized_object = ExampleObject {
         prop_a: "".to_string(),
         prop_b: false,
         prop_c: true,
