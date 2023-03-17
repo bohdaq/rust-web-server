3d9442236d0eab1eb89094ceefdfe79e5ce0ce43
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-17 20:01:40 +0200

    wip

--- src/json/array/tests.rs
+++ src/json/array/tests.rs
@@ -19,8 +19,13 @@ fn vector_to_json() {
     let actual = JSONArrayOfObjects::<ExampleObject>::to_json(vec![obj, obj2]).unwrap();
     let expected = "[{\r\n  \"prop_a\": \"\",\r\n  \"prop_b\": false,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 0,\r\n  \"prop_e\": 0\r\n},\r\n{\r\n  \"prop_a\": \"test\",\r\n  \"prop_b\": true,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 10,\r\n  \"prop_e\": 2.2\r\n}]".to_string();
     assert_eq!(actual, expected);
+}
 
-    let parsed_list = JSONArrayOfObjects::<ExampleObject>::from_json(actual).unwrap();
+#[test]
+fn json_to_vector() {
+    let json = "[{\r\n  \"prop_a\": \"\",\r\n  \"prop_b\": false,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 0,\r\n  \"prop_e\": 0\r\n},\r\n{\r\n  \"prop_a\": \"test\",\r\n  \"prop_b\": true,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 10,\r\n  \"prop_e\": 2.2\r\n}]".to_string();
+
+    let parsed_list = JSONArrayOfObjects::<ExampleObject>::from_json(json).unwrap();
     assert_eq!(2, parsed_list.len());
 
     let parsed_obj = parsed_list.get(0).unwrap();
