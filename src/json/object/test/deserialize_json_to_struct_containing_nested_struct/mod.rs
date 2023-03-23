0f3ed02cb89883ff9ef1600ea1a4adee319fdb55
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-23 22:52:05 +0200

    wip

--- src/json/object/test/deserialize_json_to_struct_containing_nested_struct/mod.rs
+++ src/json/object/test/deserialize_json_to_struct_containing_nested_struct/mod.rs
@@ -1,3 +1,4 @@
+use file_ext::FileExt;
 use crate::json::object::test::deserialize_json_to_struct_containing_nested_struct::nested_object::NestedObject;
 use crate::json::object::test::deserialize_json_to_struct_containing_nested_struct::some_object::SomeObject;
 use crate::json::object::{FromJSON, ToJSON};
@@ -18,7 +19,14 @@ fn deserialize_json_to_struct_containing_nested_struct() {
     };
 
     let json_string = obj.to_json_string();
-    let expected_json_string = "{\r\n  \"prop_a\": \"123abc\",\r\n  \"prop_b\": true,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 4356257,\r\n  \"prop_e\": 4356.257,\r\n  \"prop_f\": {\r\n  \"prop_foo\": true\r\n}\r\n}";
+    // 'to_json_string' does not perform indentation on nested objects and arrays
+    // take a look at 'some-object-formatted.json' for properly indented human readable json
+    let path = FileExt::build_path(&["src", "json", "object", "test", "deserialize_json_to_struct_containing_nested_struct", "some-object.json"]);
+    let pwd = FileExt::working_directory().unwrap();
+
+    let file_path = FileExt::build_path(&[pwd.as_str(), path.as_str()]);
+    let file_as_bytes = FileExt::read_file(file_path.as_str()).unwrap();
+    let expected_json_string = String::from_utf8(file_as_bytes).unwrap();
 
     assert_eq!(expected_json_string, json_string);
 
