8f50ca70a454d110f1f689c8dee7bc77d7f3d033
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-26 01:37:02 +0200

    wip

--- src/json/object/test/deserialize_json_with_nested_object_null_field_to_struct/mod.rs
+++ src/json/object/test/deserialize_json_with_nested_object_null_field_to_struct/mod.rs
@@ -1,3 +1,4 @@
+use file_ext::FileExt;
 use crate::json::object::FromJSON;
 use crate::json::object::test::deserialize_json_with_nested_object_null_field_to_struct::some_object::SomeObject;
 
@@ -6,7 +7,12 @@ mod some_object;
 
 #[test]
 fn parse_nested_object_property_null() {
-    let json_string = "{\r\n  \"prop_a\": \"123abc\",\r\n  \"prop_b\": true,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 4356257,\r\n  \"prop_e\": 4356.257,\r\n  \"prop_f\": {\r\n  \"prop_foo\": null\r\n}\r\n}";
+    let path = FileExt::build_path(&["src", "json", "object", "test", "deserialize_json_with_nested_object_null_field_to_struct", "some-object.json"]);
+    let pwd = FileExt::working_directory().unwrap();
+
+    let file_path = FileExt::build_path(&[pwd.as_str(), path.as_str()]);
+    let file_as_bytes = FileExt::read_file(file_path.as_str()).unwrap();
+    let json_string = String::from_utf8(file_as_bytes).unwrap();
 
 
     let mut deserealized_object = SomeObject {
