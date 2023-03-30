dbe16915bb2ce7c23f035beb93731d549d619f90
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-03-30 20:54:22 +0300

    wip

--- src/json/object/tests/example/some_object.rs
+++ src/json/object/tests/example/some_object.rs
@@ -4,55 +4,21 @@ use crate::json::object::{FromJSON, JSON, ToJSON};
 use crate::json::property::JSONProperty;
 use crate::symbol::SYMBOL;
 
+// define your struct
 pub struct SomeObject {
     pub prop_a: String,
     pub prop_b: bool
 }
 
 impl New for SomeObject {
+    // initiate struct with default values
     fn new() -> Self {
         SomeObject { prop_a: "".to_string(), prop_b: false }
     }
 }
 
-impl FromJSON for SomeObject {
-    fn parse_json_to_properties(&self, json_string: String) -> Result<Vec<(JSONProperty, JSONValue)>, String> {
-        let boxed_parse = JSON::parse_as_properties(json_string);
-        if boxed_parse.is_err() {
-            let message = boxed_parse.err().unwrap();
-            return Err(message)
-        }
-        let properties = boxed_parse.unwrap();
-        Ok(properties)
-    }
-    fn set_properties(&mut self, properties: Vec<(JSONProperty, JSONValue)>) -> Result<(), String> {
-        for (property, value) in properties {
-            if property.property_name == "prop_a" {
-                self.prop_a = value.string.unwrap();
-            }
-            if property.property_name == "prop_b" {
-                self.prop_b = value.bool.unwrap();
-            }
-        }
-        Ok(())
-    }
-    fn parse(&mut self, json_string: String) -> Result<(), String> {
-        let boxed_properties = self.parse_json_to_properties(json_string);
-        if boxed_properties.is_err() {
-            let message = boxed_properties.err().unwrap();
-            return Err(message);
-        }
-        let properties = boxed_properties.unwrap();
-        let boxed_set = self.set_properties(properties);
-        if boxed_set.is_err() {
-            let message = boxed_set.err().unwrap();
-            return Err(message);
-        }
-        Ok(())
-    }
-}
-
 impl ToJSON for SomeObject {
+    // here you need to list fields used in your struct
     fn list_properties() -> Vec<JSONProperty> {
         let mut list = vec![];
 
@@ -65,6 +31,7 @@ impl ToJSON for SomeObject {
         list
     }
 
+    // here you need to use fields used in your struct
     fn get_property(&self, property_name: String) -> JSONValue {
         let mut value = JSONValue::new();
 
@@ -81,6 +48,7 @@ impl ToJSON for SomeObject {
         value
     }
 
+    // change SomeObject to your struct, update nested if statements in for loop according to your struct fields
     fn to_json_string(&self) -> String {
         let mut json_list = vec![];
         json_list.push(SYMBOL.opening_curly_bracket.to_string());
@@ -113,5 +81,47 @@ impl ToJSON for SomeObject {
         json_list.push(SYMBOL.closing_curly_bracket.to_string());
         let json= json_list.join(SYMBOL.new_line_carriage_return);
         json
+    }
+}
+
+impl FromJSON for SomeObject {
+    // can be copy-pasted
+    fn parse_json_to_properties(&self, json_string: String) -> Result<Vec<(JSONProperty, JSONValue)>, String> {
+        let boxed_parse = JSON::parse_as_properties(json_string);
+        if boxed_parse.is_err() {
+            let message = boxed_parse.err().unwrap();
+            return Err(message)
+        }
+        let properties = boxed_parse.unwrap();
+        Ok(properties)
+    }
+
+    // here you need to change if statements inside for loop corresponding to your struct fields
+    fn set_properties(&mut self, properties: Vec<(JSONProperty, JSONValue)>) -> Result<(), String> {
+        for (property, value) in properties {
+            if property.property_name == "prop_a" {
+                self.prop_a = value.string.unwrap();
+            }
+            if property.property_name == "prop_b" {
+                self.prop_b = value.bool.unwrap();
+            }
+        }
+        Ok(())
+    }
+
+    // can be copy-pasted
+    fn parse(&mut self, json_string: String) -> Result<(), String> {
+        let boxed_properties = self.parse_json_to_properties(json_string);
+        if boxed_properties.is_err() {
+            let message = boxed_properties.err().unwrap();
+            return Err(message);
+        }
+        let properties = boxed_properties.unwrap();
+        let boxed_set = self.set_properties(properties);
+        if boxed_set.is_err() {
+            let message = boxed_set.err().unwrap();
+            return Err(message);
+        }
+        Ok(())
     }
 }
