c67a289078116d54557676a5b9cdfd82eef48701
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-04-03 06:09:45 +0300

    wip

--- src/json/object/tests/deserialize_json_extra_comma_after_boolean_true_property/mod.rs
+++ src/json/object/tests/deserialize_json_extra_comma_after_boolean_true_property/mod.rs
@@ -1,6 +1,9 @@
+mod example_object;
+mod example_nested_object;
+
 use file_ext::FileExt;
-use crate::json::example_object::ExampleObject;
 use crate::json::object::{FromJSON};
+use crate::json::object::tests::deserialize_json_extra_comma_after_boolean_true_property::example_object::ExampleObject;
 
 #[test]
 fn deserialize_json_extra_comma_after_boolean_true_property() {
