66a2acc225ac64a3b98b6953246e3767e27f9eb1
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-04-05 03:48:35 +0300

    wip

--- src/json/object/tests/deserialize_json_missing_comma_after_string/mod.rs
+++ src/json/object/tests/deserialize_json_missing_comma_after_string/mod.rs
@@ -1,6 +1,9 @@
+mod example_nested_object;
+mod example_object;
+
 use file_ext::FileExt;
-use crate::json::example_object::ExampleObject;
 use crate::json::object::{FromJSON};
+use crate::json::object::tests::deserialize_json_missing_comma_after_string::example_object::ExampleObject;
 
 #[test]
 fn deserialize_json_missing_comma_after_string() {
