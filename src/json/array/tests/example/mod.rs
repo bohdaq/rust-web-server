4f6dd6b41752e6da8f3b47186e32eb2d04fe5a0d
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-04-07 02:16:31 +0300

    wip

--- src/json/array/tests/example/mod.rs
+++ src/json/array/tests/example/mod.rs
@@ -1,7 +1,7 @@
 mod example_object;
 mod example_nested_object;
 
-use crate::json::array::{JSONArrayOfObjects, New};
+use crate::json::array::{New};
 use crate::json::array::tests::example::example_object::ExampleObject;
 
 #[test]
@@ -32,7 +32,7 @@ fn vector_to_json() {
 fn json_to_vector() {
     let json = "[{\r\n  \"prop_a\": \"\",\r\n  \"prop_b\": false,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 0,\r\n  \"prop_e\": 0\r\n},\r\n{\r\n  \"prop_a\": \"test\",\r\n  \"prop_b\": true,\r\n  \"prop_c\": false,\r\n  \"prop_d\": 10,\r\n  \"prop_e\": 2.2\r\n}]".to_string();
 
-    let parsed_list : Vec<ExampleObject> = JSONArrayOfObjects::<ExampleObject>::from_json(json).unwrap();
+    let parsed_list : Vec<ExampleObject> = ExampleObject::from_json_list(json).unwrap();
     assert_eq!(2, parsed_list.len());
 
     let parsed_obj = parsed_list.get(0).unwrap();
