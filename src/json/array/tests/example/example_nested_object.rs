be6fda731c8908ca14d18830e9a6d686c8875a2c
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-04-07 22:26:39 +0300

    wip

--- src/json/array/tests/example/example_nested_object.rs
+++ src/json/array/tests/example/example_nested_object.rs
@@ -3,6 +3,7 @@ use crate::json::array::New;
 use crate::json::object::{FromJSON, JSON, ToJSON};
 use crate::json::property::JSONProperty;
 
+// define your struct
 pub struct ExampleNestedObject {
     pub prop_a: String,
     pub prop_b: bool,
@@ -11,6 +12,7 @@ pub struct ExampleNestedObject {
 }
 
 impl New for ExampleNestedObject {
+    // initiate struct with default values
     fn new() -> Self {
         ExampleNestedObject {
             prop_a: "".to_string(),
@@ -21,7 +23,72 @@ impl New for ExampleNestedObject {
     }
 }
 
+impl ToJSON for ExampleNestedObject {
+    // here you need to list fields used in your struct
+    fn list_properties() -> Vec<JSONProperty> {
+        let mut list = vec![];
+
+        let property = JSONProperty { property_name: "prop_a".to_string(), property_type: JSON_TYPE.string.to_string() };
+        list.push(property);
+
+        let property = JSONProperty { property_name: "prop_b".to_string(), property_type: JSON_TYPE.boolean.to_string() };
+        list.push(property);
+
+
+        let property = JSONProperty { property_name: "prop_d".to_string(), property_type: JSON_TYPE.integer.to_string() };
+        list.push(property);
+
+        let property = JSONProperty { property_name: "prop_e".to_string(), property_type: JSON_TYPE.number.to_string() };
+        list.push(property);
+
+        list
+    }
+
+    // here you need to use fields used in your struct
+    fn get_property(&self, property_name: String) -> JSONValue {
+        let mut value = JSONValue::new();
+
+        if property_name == "prop_a".to_string() {
+            let string : String = self.prop_a.to_owned();
+            value.string = Some(string);
+        }
+
+        if property_name == "prop_b".to_string() {
+            let boolean : bool = self.prop_b;
+            value.bool = Some(boolean);
+        }
+
+
+        if property_name == "prop_d".to_string() {
+            let integer : i128 = self.prop_c;
+            value.i128 = Some(integer);
+        }
+
+        if property_name == "prop_e".to_string() {
+            let floating_point_number: f64 = self.prop_d;
+            value.f64 = Some(floating_point_number);
+        }
+
+        value
+    }
+
+    // change ExampleObject to your struct, update nested if statements in for loop according to your struct fields
+    fn to_json_string(&self) -> String {
+        let mut processed_data = vec![];
+
+        let properties = ExampleNestedObject::list_properties();
+        for property in properties {
+            let value = self.get_property(property.property_name.to_string());
+            processed_data.push((property, value));
+
+        }
+
+        JSON::to_json_string(processed_data)
+    }
+}
+
 impl FromJSON for ExampleNestedObject {
+    // can be copy-pasted
     fn parse_json_to_properties(&self, json_string: String) -> Result<Vec<(JSONProperty, JSONValue)>, String> {
         let boxed_parse = JSON::parse_as_properties(json_string);
         if boxed_parse.is_err() {
@@ -31,6 +98,8 @@ impl FromJSON for ExampleNestedObject {
         let properties = boxed_parse.unwrap();
         Ok(properties)
     }
+
+    // here you need to change if statements inside for loop corresponding to your struct fields
     fn set_properties(&mut self, properties: Vec<(JSONProperty, JSONValue)>) -> Result<(), String> {
         for (property, value) in properties {
             if property.property_name == "prop_a" {
@@ -60,6 +129,8 @@ impl FromJSON for ExampleNestedObject {
         }
         Ok(())
     }
+
+    // can be copy-pasted
     fn parse(&mut self, json_string: String) -> Result<(), String> {
         let boxed_properties = self.parse_json_to_properties(json_string);
         if boxed_properties.is_err() {
@@ -73,66 +144,5 @@ impl FromJSON for ExampleNestedObject {
             return Err(message);
         }
         Ok(())
-    }
-}
-
-impl ToJSON for ExampleNestedObject {
-    fn list_properties() -> Vec<JSONProperty> {
-        let mut list = vec![];
-
-        let property = JSONProperty { property_name: "prop_a".to_string(), property_type: JSON_TYPE.string.to_string() };
-        list.push(property);
-
-        let property = JSONProperty { property_name: "prop_b".to_string(), property_type: JSON_TYPE.boolean.to_string() };
-        list.push(property);
-
-
-        let property = JSONProperty { property_name: "prop_d".to_string(), property_type: JSON_TYPE.integer.to_string() };
-        list.push(property);
-
-        let property = JSONProperty { property_name: "prop_e".to_string(), property_type: JSON_TYPE.number.to_string() };
-        list.push(property);
-
-        list
-    }
-
-    fn get_property(&self, property_name: String) -> JSONValue {
-        let mut value = JSONValue::new();
-
-        if property_name == "prop_a".to_string() {
-            let string : String = self.prop_a.to_owned();
-            value.string = Some(string);
-        }
-
-        if property_name == "prop_b".to_string() {
-            let boolean : bool = self.prop_b;
-            value.bool = Some(boolean);
-        }
-
-
-        if property_name == "prop_d".to_string() {
-            let integer : i128 = self.prop_c;
-            value.i128 = Some(integer);
-        }
-
-        if property_name == "prop_e".to_string() {
-            let floating_point_number: f64 = self.prop_d;
-            value.f64 = Some(floating_point_number);
-        }
-
-        value
-    }
-
-    fn to_json_string(&self) -> String {
-        let mut processed_data = vec![];
-
-        let properties = ExampleNestedObject::list_properties();
-        for property in properties {
-            let value = self.get_property(property.property_name.to_string());
-            processed_data.push((property, value));
-
-        }
-
-        JSON::to_json_string(processed_data)
     }
 }
