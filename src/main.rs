00b7c7d1709a378fff2fb9cd5811f85a1fd2f357
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-04-26 20:37:44 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -67,71 +67,73 @@ pub fn create_tcp_listener_with_thread_pool(ip: &str, port: i32, thread_count: i
     if boxed_listener.is_err() {
         eprintln!("unable to set up TCP listener: {}", boxed_listener.err().unwrap());
         return;
-    } else {
-        let listener = boxed_listener.unwrap();
-        let pool = ThreadPool::new(thread_count as usize);
-
-
-        let server_url_thread_count = Log::server_url_thread_count("http", &bind_addr, thread_count);
-        println!("{}", server_url_thread_count);
-
-
-        for boxed_stream in listener.incoming() {
-            if boxed_stream.is_err() {
-                eprintln!("unable to get TCP stream: {}", boxed_stream.err().unwrap());
-                return;
-            } else {
-                let stream = boxed_stream.unwrap();
-
-                print!("Connection established, ");
-
-                let boxed_local_addr = stream.local_addr();
-                if boxed_local_addr.is_ok() {
-                    print!("local addr: {}", boxed_local_addr.unwrap())
-                } else {
-                    eprintln!("\nunable to read local addr");
-                    return;
-                }
-
-                let boxed_peer_addr = stream.peer_addr();
-                if boxed_peer_addr.is_err() {
-                    eprintln!("\nunable to read peer addr");
-                    return;
-                }
-                let peer_addr = boxed_peer_addr.unwrap();
-                print!(", peer addr: {}\n", peer_addr.to_string());
-
-                let (server_ip, server_port, _thread_count) = get_ip_port_thread_count();
-                let client_ip = peer_addr.ip().to_string();
-                let client_port = peer_addr.port() as i32;
-                let request_allocation_size = get_request_allocation_size();
-
-                let connection = ConnectionInfo {
-                    client: Address {
-                        ip: client_ip.to_string(),
-                        port: client_port
-                    },
-                    server: Address {
-                        ip: server_ip,
-                        port: server_port
-                    },
-                    request_size: request_allocation_size,
-                };
-
-
-
-                pool.execute(move || {
-                    let app = App::new();
-
-                    let boxed_process = Server::process(stream, connection, app);
-                    if boxed_process.is_err() {
-                        let message = boxed_process.err().unwrap();
-                        eprintln!("{}", message);
-                    }
-                });
-            }
-        }
     }
+
+    let listener = boxed_listener.unwrap();
+    let pool = ThreadPool::new(thread_count as usize);
+
+
+    let server_url_thread_count = Log::server_url_thread_count("http", &bind_addr, thread_count);
+    println!("{}", server_url_thread_count);
+
+
+    for boxed_stream in listener.incoming() {
+        if boxed_stream.is_err() {
+            eprintln!("unable to get TCP stream: {}", boxed_stream.err().unwrap());
+            return;
+        }
+
+        let stream = boxed_stream.unwrap();
+
+        print!("Connection established, ");
+
+        let boxed_local_addr = stream.local_addr();
+        if boxed_local_addr.is_ok() {
+            print!("local addr: {}", boxed_local_addr.unwrap())
+        } else {
+            eprintln!("\nunable to read local addr");
+            return;
+        }
+
+        let boxed_peer_addr = stream.peer_addr();
+        if boxed_peer_addr.is_err() {
+            eprintln!("\nunable to read peer addr");
+            return;
+        }
+        let peer_addr = boxed_peer_addr.unwrap();
+        print!(", peer addr: {}\n", peer_addr.to_string());
+
+        let (server_ip, server_port, _thread_count) = get_ip_port_thread_count();
+        let client_ip = peer_addr.ip().to_string();
+        let client_port = peer_addr.port() as i32;
+        let request_allocation_size = get_request_allocation_size();
+
+        let connection = ConnectionInfo {
+            client: Address {
+                ip: client_ip.to_string(),
+                port: client_port
+            },
+            server: Address {
+                ip: server_ip,
+                port: server_port
+            },
+            request_size: request_allocation_size,
+        };
+
+
+
+        pool.execute(move || {
+            let app = App::new();
+
+            let boxed_process = Server::process(stream, connection, app);
+            if boxed_process.is_err() {
+                let message = boxed_process.err().unwrap();
+                eprintln!("{}", message);
+            }
+        });
+
+    }
+
 
 }
 
