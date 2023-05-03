de9959185f53b5381b1000d273936e512a271dfa
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-05-03 16:42:50 +0300

    wip

--- src/body/example/mod.rs
+++ src/body/example/mod.rs
@@ -1,5 +1,8 @@
 use crate::http::VERSION;
+use crate::mime_type::MimeType;
+use crate::range::{ContentRange, Range};
 use crate::request::{METHOD, Request};
+use crate::response::{Response, STATUS_CODE_REASON_PHRASE};
 
 #[test]
 fn body_in_request() {
@@ -16,6 +19,35 @@ fn body_in_request() {
 
     // replace with your logic
     assert_eq!(Vec::from("request body can be anythings"), request.body);
+}
+
+#[test]
+fn body_in_response() {
+    // can be any piece of data
+    let body : Vec<u8> = Vec::from("request body can be anythings");
+    let start = 0;
+    let length = body.len();
+
+    let content_range = ContentRange {
+        unit: Range::BYTES.to_string(),
+        range: Range { start: start, end: length as u64 },
+        size: length.to_string(),
+        body,
+        content_type: MimeType::TEXT_PLAIN.to_string(),
+    };
+
+    let response = Response {
+        http_version: VERSION.http_1_1.to_string(),
+        status_code: *STATUS_CODE_REASON_PHRASE.n200_ok.status_code,
+        reason_phrase: STATUS_CODE_REASON_PHRASE.n200_ok.reason_phrase.to_string(),
+        headers: vec![],
+        content_range_list: vec![content_range],
+    };
+
+    let response_body : &ContentRange = response.content_range_list.get(0).unwrap();
+
+    // replace with your logic
+    assert_eq!(Vec::from("request body can be anythings"), response_body.body);
 }
 
 
