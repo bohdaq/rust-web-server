284047f310dd64ca53984f1b3e92041a3a135344
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-05-10 12:45:19 +0300

    wip

--- src/url/mod.rs
+++ src/url/mod.rs
@@ -10,4 +10,12 @@ impl URL {
 
         return _result
     }
+
+    pub fn decode_uri_component(component: &str) -> String {
+        let mut _result = component.replace( "%20", SYMBOL.whitespace);
+        _result = component.replace("%0A", SYMBOL.new_line);
+        _result = component.replace ("%0D", SYMBOL.carriage_return);
+
+        return _result
+    }
 }
