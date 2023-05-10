7eeb7e89bfda664951972019935f766820997aff
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-05-10 13:44:25 +0300

    wip

--- src/url/mod.rs
+++ src/url/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use crate::symbol::SYMBOL;
 
 pub struct URL;
@@ -5,16 +8,16 @@ pub struct URL;
 impl URL {
     pub fn encode_uri_component(component: &str) -> String {
         let mut _result = component.replace(SYMBOL.whitespace, "%20");
-        _result = component.replace(SYMBOL.new_line, "%0A");
-        _result = component.replace(SYMBOL.carriage_return, "%0D");
+        _result = _result.replace(SYMBOL.carriage_return, "%0D");
+        _result = _result.replace(SYMBOL.new_line, "%0A");
 
         return _result
     }
 
     pub fn decode_uri_component(component: &str) -> String {
         let mut _result = component.replace( "%20", SYMBOL.whitespace);
-        _result = component.replace("%0A", SYMBOL.new_line);
-        _result = component.replace ("%0D", SYMBOL.carriage_return);
+        _result = _result.replace("%0A", SYMBOL.new_line);
+        _result = _result.replace ("%0D", SYMBOL.carriage_return);
 
         return _result
     }
---
+++ src/url/tests.rs
@@ -0,0 +1,11 @@
+use crate::url::URL;
+
+#[test]
+fn encode_decode() {
+    let component = "\r\n  \r\n";
+    let mut _result = URL::encode_uri_component(component);
+    assert_eq!("%0D%0A%20%20%0D%0A", _result);
+    _result = URL::decode_uri_component(_result.as_str());
+    assert_eq!(component, _result);
+
+}
