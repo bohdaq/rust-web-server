7959f0280e197f870793ad8356d003461fbf7ec5
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-05-11 13:21:55 +0300

    wip

--- src/url/mod.rs
+++ src/url/mod.rs
@@ -1,64 +1,17 @@
 #[cfg(test)]
 mod tests;
 
-use crate::symbol::SYMBOL;
+use url_search_params::encode_uri_component;
+use url_search_params::decode_uri_component;
 
 pub struct URL;
 
 impl URL {
-    pub fn encode_uri_component(component: &str) -> String {
-        let mut _result = component.replace(SYMBOL.percent, "%25");
-        _result = _result.replace(SYMBOL.whitespace, "%20");
-        _result = _result.replace(SYMBOL.carriage_return, "%0D");
-        _result = _result.replace(SYMBOL.new_line, "%0A");
-        _result = _result.replace(SYMBOL.exclamation_mark, "%21");
-        _result = _result.replace(SYMBOL.quotation_mark, "%22");
-        _result = _result.replace(SYMBOL.number_sign, "%23");
-        _result = _result.replace(SYMBOL.dollar, "%24");
-        _result = _result.replace(SYMBOL.ampersand, "%26");
-        _result = _result.replace(SYMBOL.single_quote, "%27");
-        _result = _result.replace(SYMBOL.opening_bracket, "%28");
-        _result = _result.replace(SYMBOL.closing_bracket, "%29");
-        _result = _result.replace(SYMBOL.asterisk, "%2A");
-        _result = _result.replace(SYMBOL.plus, "%2B");
-        _result = _result.replace(SYMBOL.comma, "%2C");
-        _result = _result.replace(SYMBOL.slash, "%2F");
-        _result = _result.replace(SYMBOL.colon, "%3A");
-        _result = _result.replace(SYMBOL.semicolon, "%3B");
-        _result = _result.replace(SYMBOL.equals, "%3D");
-        _result = _result.replace(SYMBOL.at, "%40");
-        _result = _result.replace(SYMBOL.opening_square_bracket, "%5B");
-        _result = _result.replace(SYMBOL.closing_square_bracket, "%5D");
-
-
-        return _result
+    pub fn percent_encode(component: &str) -> String {
+        encode_uri_component(component)
     }
 
-    pub fn decode_uri_component(component: &str) -> String {
-        let mut _result = component.replace( "%20", SYMBOL.whitespace);
-        _result = _result.replace("%0A", SYMBOL.new_line);
-        _result = _result.replace ("%0D", SYMBOL.carriage_return);
-        _result = _result.replace ("%21", SYMBOL.exclamation_mark);
-        _result = _result.replace ("%22", SYMBOL.quotation_mark);
-        _result = _result.replace ("%23", SYMBOL.number_sign);
-        _result = _result.replace ("%24", SYMBOL.dollar);
-        _result = _result.replace ("%25", SYMBOL.percent);
-        _result = _result.replace ("%26", SYMBOL.ampersand);
-        _result = _result.replace ("%27", SYMBOL.single_quote);
-        _result = _result.replace ("%28", SYMBOL.opening_bracket);
-        _result = _result.replace ("%29", SYMBOL.closing_bracket);
-        _result = _result.replace ("%2A", SYMBOL.asterisk);
-        _result = _result.replace ("%2B", SYMBOL.plus);
-        _result = _result.replace ("%2C", SYMBOL.comma);
-        _result = _result.replace ("%2F", SYMBOL.slash);
-        _result = _result.replace ("%3A", SYMBOL.colon);
-        _result = _result.replace ("%3B", SYMBOL.semicolon);
-        _result = _result.replace ("%3D", SYMBOL.equals);
-        _result = _result.replace ("%3F", SYMBOL.question_mark);
-        _result = _result.replace ("%40", SYMBOL.at);
-        _result = _result.replace ("%5B", SYMBOL.opening_square_bracket);
-        _result = _result.replace ("%5D", SYMBOL.closing_square_bracket);
-
-        return _result
+    pub fn percent_decode(component: &str) -> String {
+        decode_uri_component(component)
     }
 }
--- src/url/tests.rs
+++ src/url/tests.rs
@@ -3,8 +3,8 @@ use crate::url::URL;
 #[test]
 fn encode_decode() {
     let component = "\r\n \"%!#$&'()*+,/:;=?@[]][@?=;:/,+*)('&$#!%\" \r\n";
-    let mut _result = URL::encode_uri_component(component);
+    let mut _result = URL::percent_encode(component);
     assert_eq!("%0D%0A%20%22%25%21%23%24%26%27%28%29%2A%2B%2C%2F%3A%3B%3D?%40%5B%5D%5D%5B%40?%3D%3B%3A%2F%2C%2B%2A%29%28%27%26%24%23%21%25%22%20%0D%0A", _result);
-    _result = URL::decode_uri_component(_result.as_str());
+    _result = URL::percent_decode(_result.as_str());
     assert_eq!(component, _result);
 }
