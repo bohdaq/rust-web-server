eba289dd2c7480c0996a32f62343f189c13de503
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-05-17 15:23:59 +0300

    wip

--- src/app/controller/favicon/mod.rs
+++ src/app/controller/favicon/mod.rs
@@ -1,17 +1,67 @@
 use file_ext::FileExt;
+use crate::controller::Controller;
 
 use crate::mime_type::MimeType;
 use crate::range::Range;
-use crate::request::Request;
+use crate::request::{METHOD, Request};
 use crate::response::{Response, STATUS_CODE_REASON_PHRASE};
+use crate::server::ConnectionInfo;
 
 pub struct FaviconController;
 
+impl Controller for FaviconController {
+    fn is_matching(request: &Request, _connection: &ConnectionInfo) -> bool {
+        request.method == METHOD.get && request.request_uri == "/favicon.svg"
+    }
+
+    fn process(_request: &Request, mut response: Response, _connection: &ConnectionInfo) -> Response {
+        response.status_code = *STATUS_CODE_REASON_PHRASE.n200_ok.status_code;
+        response.reason_phrase = STATUS_CODE_REASON_PHRASE.n200_ok.reason_phrase.to_string();
+
+
+        if FileExt::does_file_exist(FaviconController::FAVICON_FILEPATH) {
+            let boxed_content_range =
+                Range::get_content_range_of_a_file(FaviconController::FAVICON_FILEPATH);
+
+            if boxed_content_range.is_ok() {
+                let content_range = boxed_content_range.unwrap();
+                let content_range_list = vec![content_range];
+                response.content_range_list = content_range_list;
+            } else {
+                let error = boxed_content_range.err().unwrap();
+                let mime_type = MimeType::TEXT_HTML.to_string();
+                let content_range = Range::get_content_range(
+                    Vec::from(error.as_bytes()),
+                    mime_type
+                );
+
+                let content_range_list = vec![content_range];
+                response.content_range_list = content_range_list;
+                response.status_code = *STATUS_CODE_REASON_PHRASE.n500_internal_server_error.status_code;
+                response.reason_phrase = STATUS_CODE_REASON_PHRASE.n500_internal_server_error.reason_phrase.to_string();
+            }
+        } else {
+            let style_file = include_bytes!("favicon.svg");
+
+            let content_range =
+                Range::get_content_range(style_file.to_vec(), MimeType::IMAGE_SVG.to_string());
+
+
+            let content_range_list = vec![content_range];
+            response.content_range_list = content_range_list;
+
+        }
+
+        response
+    }
+}
+
+//backward compatability
 impl FaviconController {
     pub const FAVICON_FILEPATH: &'static str = "favicon.svg";
 
     pub fn is_matching_request(request: &Request) -> bool {
-        request.request_uri == "/favicon.svg"
+        request.method == METHOD.get && request.request_uri == "/favicon.svg"
     }
 
     pub fn process_request(_request: &Request, mut response: Response) -> Response {
