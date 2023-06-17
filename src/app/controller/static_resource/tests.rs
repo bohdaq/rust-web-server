158c27de89ef6fa0bc42cd23105adc60e9d45a07
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-06-17 04:09:09 +0300

    wip

--- src/app/controller/static_resource/tests.rs
+++ src/app/controller/static_resource/tests.rs
@@ -4,7 +4,7 @@ use crate::controller::Controller;
 use crate::core::New;
 use crate::http::VERSION;
 use crate::request::{METHOD, Request};
-use crate::response::Response;
+use crate::response::{Response, STATUS_CODE_REASON_PHRASE};
 use crate::server::{Address, ConnectionInfo};
 
 #[test]
@@ -14,6 +14,97 @@ fn file_retrieval() {
     let request = Request {
         method: METHOD.get.to_string(),
         request_uri: path.to_string(),
+        http_version: VERSION.http_1_1.to_string(),
+        headers: vec![],
+        body: vec![],
+    };
+
+    let connection_info = ConnectionInfo {
+        client: Address { ip: "127.0.0.1".to_string(), port: 0 },
+        server: Address { ip: "127.0.0.1".to_string(), port: 0 },
+        request_size: 0,
+    };
+
+    let is_matching = StaticResourceController::is_matching(&request, &connection_info);
+    assert!(is_matching);
+
+    let mut response = Response::new();
+    response = StaticResourceController::process(&request, response, &connection_info);
+
+
+    let path_array = vec!["static", "test.txt"];
+    let path = FileExt::build_path(&path_array);
+    let expected_text = FileExt::read_file(path.as_str()).unwrap();
+
+    let actual_text = response.content_range_list.get(0).unwrap().body.to_vec();
+    assert_eq!(actual_text, expected_text);
+}
+
+#[test]
+fn not_found() {
+    let path = "/not_found";
+
+    let request = Request {
+        method: METHOD.get.to_string(),
+        request_uri: path.to_string(),
+        http_version: VERSION.http_1_1.to_string(),
+        headers: vec![],
+        body: vec![],
+    };
+
+    let connection_info = ConnectionInfo {
+        client: Address { ip: "127.0.0.1".to_string(), port: 0 },
+        server: Address { ip: "127.0.0.1".to_string(), port: 0 },
+        request_size: 0,
+    };
+
+    let is_matching = StaticResourceController::is_matching(&request, &connection_info);
+    assert!(!is_matching);
+
+    let mut response = Response::new();
+    response = StaticResourceController::process(&request, response, &connection_info);
+
+
+    assert_eq!(response.status_code, *STATUS_CODE_REASON_PHRASE.n501_not_implemented.status_code);
+    assert_eq!(response.reason_phrase, STATUS_CODE_REASON_PHRASE.n501_not_implemented.reason_phrase);
+}
+
+#[test]
+fn malformed() {
+    let path = "//randomtext";
+
+    let request = Request {
+        method: METHOD.get.to_string(),
+        request_uri: path.to_string(),
+        http_version: VERSION.http_1_1.to_string(),
+        headers: vec![],
+        body: vec![],
+    };
+
+    let connection_info = ConnectionInfo {
+        client: Address { ip: "127.0.0.1".to_string(), port: 0 },
+        server: Address { ip: "127.0.0.1".to_string(), port: 0 },
+        request_size: 0,
+    };
+
+    let is_matching = StaticResourceController::is_matching(&request, &connection_info);
+    assert!(!is_matching);
+
+    let mut response = Response::new();
+    response = StaticResourceController::process(&request, response, &connection_info);
+
+
+    assert_eq!(response.status_code, *STATUS_CODE_REASON_PHRASE.n501_not_implemented.status_code);
+    assert_eq!(response.reason_phrase, STATUS_CODE_REASON_PHRASE.n501_not_implemented.reason_phrase);
+}
+
+#[test]
+fn file_retrieval_with_query_params() {
+    let request_uri = "/static/test.txt?param=as%20df&another_param=1234";
+
+    let request = Request {
+        method: METHOD.get.to_string(),
+        request_uri: request_uri.to_string(),
         http_version: VERSION.http_1_1.to_string(),
         headers: vec![],
         body: vec![],
