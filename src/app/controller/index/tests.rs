8b31a4f241009ba402375f0e0ff9bb9bc23783fa
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-06-26 06:46:01 +0300

    wip

--- src/app/controller/index/tests.rs
+++ src/app/controller/index/tests.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use file_ext::FileExt;
 use crate::app::controller::index::IndexController;
 use crate::controller::Controller;
@@ -43,24 +42,9 @@ fn file_retrieval() {
     let actual_text = response.content_range_list.get(0).unwrap().body.to_vec();
     assert_eq!(actual_text, expected_text.to_vec());
 
-
-    let mut label = "".to_string();
-
-    // default index.html
-    let progress_callback  = |_start, _end, _file_length| {
-        label = format!("copying block starting from byte {} to {} of total {} bytes", _start, _end, _file_length)
-    };
-
-    let cancel_callback  = |_start, _end, _file_length| {
-        // if true stops copying
-        false
-    };
-
-    copy_file(
+    FileExt::copy_file(
         vec![pwd.as_str(), "index.html"],
-        vec![pwd.as_str(), "index_copy.html"],
-        progress_callback,
-        cancel_callback
+        vec![pwd.as_str(), "index_copy.html"]
     ).unwrap();
 
     FileExt::delete_file("index.html").unwrap();
@@ -100,150 +84,4 @@ fn file_retrieval() {
         vec![pwd.as_str(), "index.html"]
     ).unwrap();
     FileExt::delete_file("index_copy.html").unwrap();
-}
-
-fn file_length(path: Vec<&str>) -> Result<u64, String> {
-    let filepath = FileExt::build_path(path.as_slice());
-    let boxed_length = fs::metadata(filepath);
-    if boxed_length.is_err() {
-        let message = boxed_length.err().unwrap().to_string();
-        return Err(message)
-    }
-    let length = boxed_length.unwrap().len();
-    Ok(length)
-}
-
-fn copy_file<F, C>(from: Vec<&str>, to: Vec<&str>, mut f: F, mut c: C)-> Result<(), String>
-  where C: FnMut(u64, u64, u64) -> bool, F: FnMut(u64, u64, u64) {
-    let boxed_length = file_length(from.clone());
-    if boxed_length.is_err() {
-        let message = boxed_length.err().unwrap();
-        return Err(message);
-    }
-
-    let file_length = boxed_length.unwrap();
-    let _100kb = 10;
-    let step = _100kb;
-    let mut start = 0;
-    let mut end = step;
-    if step >= file_length {
-        end = file_length - 1;
-    }
-
-    let mut continue_copying = true;
-    while continue_copying {
-        f(start, end, file_length);
-
-        let boxed_copy = copy_part_of_file(
-            from.clone(),
-            to.clone(),
-            start,
-            end
-        );
-
-        if boxed_copy.is_err() {
-            let message = boxed_copy.err().unwrap();
-            return Err(message);
-        }
-
-        boxed_copy.unwrap();
-
-        let copying_cancelled_by_user = c(start, end, file_length);
-        let reached_end_of_file = end == file_length - 1;
-        if reached_end_of_file || copying_cancelled_by_user {
-            continue_copying = false;
-        } else {
-            start = end + 1;
-            end = end + step;
-            if start + step >= file_length {
-                end = file_length - 1;
-            }
-        }
-
-    }
-
-    Ok(())
-}
-
-fn _copy_file(from: Vec<&str>, to: Vec<&str>) -> Result<(), String> {
-    let from_path = FileExt::build_path(&from);
-    let file_exists = FileExt::does_file_exist(from_path.as_str());
-    if !file_exists {
-        let message = format!("file at given path {} does not exist", from_path.as_str());
-        return Err(message);
-    }
-
-    let to_path = FileExt::build_path(&to);
-    let file_exists = FileExt::does_file_exist(to_path.as_str());
-    if file_exists {
-        let message = format!("file at given path {} already exists", to_path.as_str());
-        return Err(message);
-    }
-
-    let boxed_content_to_copy = FileExt::read_file(from_path.as_str());
-    if boxed_content_to_copy.is_err() {
-        let message = boxed_content_to_copy.err().unwrap();
-        return Err(message);
-    }
-    let content_to_copy = boxed_content_to_copy.unwrap();
-
-
-    let to_path = FileExt::build_path(&to);
-    if FileExt::does_file_exist(to_path.as_str()) {
-        let boxed_delete = FileExt::delete_file(to_path.as_str());
-        if boxed_delete.is_err() {
-            let message = boxed_delete.err().unwrap();
-            return Err(message);
-        }
-    }
-    let boxed_create = FileExt::create_file(to_path.as_str());
-    if boxed_create.is_err() {
-        let message = boxed_create.err().unwrap();
-        return Err(message);
-    }
-
-    let boxed_write =
-        FileExt::write_file(to_path.as_str(), content_to_copy.as_slice());
-    if boxed_write.is_err() {
-        let message = boxed_write.err().unwrap();
-        return Err(message);
-    }
-    Ok(boxed_write.unwrap())
-}
-
-
-fn copy_part_of_file(from: Vec<&str>, to: Vec<&str>, start: u64, end: u64) -> Result<(), String> {
-    let from_path = FileExt::build_path(&from);
-    let file_exists = FileExt::does_file_exist(from_path.as_str());
-    if !file_exists {
-        let message = format!("file at given path {} does not exist", from_path.as_str());
-        return Err(message);
-    }
-
-
-    let boxed_content_to_copy = FileExt::read_file_partially(from_path.as_str(), start, end);
-    if boxed_content_to_copy.is_err() {
-        let message = boxed_content_to_copy.err().unwrap();
-        return Err(message);
-    }
-    let content_to_copy = boxed_content_to_copy.unwrap();
-
-
-    let to_path = FileExt::build_path(&to);
-    if !FileExt::does_file_exist(to_path.as_str()) {
-        let boxed_create = FileExt::create_file(to_path.as_str());
-        if boxed_create.is_err() {
-            let message = boxed_create.err().unwrap();
-            return Err(message);
-        }
-    }
-
-
-    let boxed_write =
-        FileExt::write_file(to_path.as_str(), content_to_copy.as_slice());
-    if boxed_write.is_err() {
-        let message = boxed_write.err().unwrap();
-        return Err(message);
-    }
-    Ok(boxed_write.unwrap())
 }
