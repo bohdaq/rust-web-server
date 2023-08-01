cbe514cab655333364efca9b233d55ecb9f805ce
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-08-01 04:45:26 +0300

    wip

--- src/app/controller/static_resource/mod.rs
+++ src/app/controller/static_resource/mod.rs
@@ -45,7 +45,14 @@ impl Controller for StaticResourceController {
         if boxed_md.is_ok() {
             let md = boxed_md.unwrap();
             if md.is_dir() {
-                return false
+                let index_html_in_directory_array = [&static_filepath, "index.html"];
+                let index_html_in_directory = index_html_in_directory_array.join(SYMBOL.empty_string);
+
+                let boxed_file = File::open(&index_html_in_directory);
+                if boxed_file.is_err() {
+                    return false
+                }
+
             }
         }
 
@@ -262,6 +269,29 @@ impl StaticResourceController {
         let boxed_file = File::open(&static_filepath);
         if boxed_file.is_ok()  {
             let md = metadata(&static_filepath).unwrap();
+            if md.is_dir() {
+                let mut range_header = &Header {
+                    name: Header::_RANGE.to_string(),
+                    value: "bytes=0-".to_string()
+                };
+
+                let boxed_header = request.get_header(Header::_RANGE.to_string());
+                if boxed_header.is_some() {
+                    range_header = boxed_header.unwrap();
+                }
+
+                let url_array = [&request.request_uri, "index.html"];
+                let directory_index_html_path = url_array.join(SYMBOL.empty_string);
+
+                let boxed_content_range_list = Range::get_content_range_list(&directory_index_html_path, range_header);
+                if boxed_content_range_list.is_ok() {
+                    content_range_list = boxed_content_range_list.unwrap();
+                } else {
+                    let error = boxed_content_range_list.err().unwrap();
+                    return Err(error)
+                }
+            }
+
             if md.is_file() {
                 let mut range_header = &Header {
                     name: Header::_RANGE.to_string(),
